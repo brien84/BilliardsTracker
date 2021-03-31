79bcb5140b3cef6ba4e92fa5259f3836672ffc52
brien84 <houseinblues@gmail.com>
2021-03-31 23:26:30 +0300

    Create `DrillRunner`

---
+++ BilliardsTracker WatchKit Extension/DrillRunner.swift
@@ -0,0 +1,12 @@
+//
+//  DrillRunner.swift
+//  BilliardsTracker WatchKit Extension
+//
+//  Created by Marius on 2021-03-31.
+//
+
+import Foundation
+
+final class DrillRunner: ObservableObject {
+    
+}
