825eaae520745e3441999a2e50db9d388a46c762
brien84 <houseinblues@gmail.com>
2021-03-31 23:50:51 +0300

    Add pot and miss tracking

--- BilliardsTracker WatchKit Extension/DrillRunner.swift
+++ BilliardsTracker WatchKit Extension/DrillRunner.swift
@@ -8,5 +8,14 @@
 import Foundation
 
 final class DrillRunner: ObservableObject {
-    
+    @Published private(set) var potCount = 0
+    @Published private(set) var missCount = 0
+
+    func addPot() {
+        potCount += 1
+    }
+
+    func addMiss() {
+        missCount += 1
+    }
 }
