5a1b8ae0bbeb5a86c592059405a6cc0a0a79dcbe
brien84 <houseinblues@gmail.com>
2021-03-31 23:15:36 +0300

    Initial Commit

---
+++ BilliardsTracker WatchKit Extension/BilliardsTrackerApp.swift
@@ -0,0 +1,19 @@
+//
+//  BilliardsTrackerApp.swift
+//  BilliardsTracker WatchKit Extension
+//
+//  Created by Marius on 2021-03-31.
+//
+
+import SwiftUI
+
+@main
+struct BilliardsTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
---
+++ BilliardsTracker/BilliardsTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BilliardsTrackerApp.swift
+//  BilliardsTracker
+//
+//  Created by Marius on 2021-03-31.
+//
+
+import SwiftUI
+
+@main
+struct BilliardsTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
