2324ca3409d275b9bbd90aeec7c807fad9aced7b
brien84 <houseinblues@gmail.com>
2021-04-01 00:13:44 +0300

    Add drill completion tracking

--- BilliardsTracker WatchKit Extension/DrillRunner.swift
+++ BilliardsTracker WatchKit Extension/DrillRunner.swift
@@ -7,8 +7,13 @@
 
 import Foundation
 
+enum HitType {
+    case pot
+    case miss
+}
+
 final class DrillRunner: ObservableObject {
-    private let attempts = 100
+    private let attempts = 10
 
     var remainingAttempts: Int {
         attempts - potCount - missCount
@@ -17,11 +22,18 @@ final class DrillRunner: ObservableObject {
     @Published private(set) var potCount = 0
     @Published private(set) var missCount = 0
 
-    func addPot() {
-        potCount += 1
-    }
+    @Published private(set) var isCompleted = false
 
-    func addMiss() {
-        missCount += 1
+    func add(_ hit: HitType) {
+        switch hit {
+        case .pot:
+            potCount += 1
+        case .miss:
+            missCount += 1
+        }
+
+        if remainingAttempts == 0 {
+            isCompleted = true
+        }
     }
 }
