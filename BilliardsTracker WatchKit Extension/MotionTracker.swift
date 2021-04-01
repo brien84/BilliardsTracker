e321bbf240c0d6248c32d9cd30a7251836e22895
brien84 <houseinblues@gmail.com>
2021-04-01 17:08:38 +0300

    Implement `CMMotionManager`

--- BilliardsTracker WatchKit Extension/MotionTracker.swift
+++ BilliardsTracker WatchKit Extension/MotionTracker.swift
@@ -5,8 +5,34 @@
 //  Created by Marius on 2021-04-01.
 //
 
-import Foundation
+import CoreMotion
 
 final class MotionTracker {
-    
+    private lazy var motionManager: CMMotionManager = {
+        let manager = CMMotionManager()
+        manager.deviceMotionUpdateInterval = 1/40
+        return manager
+    }()
+
+    private lazy var queue: OperationQueue = {
+        let queue = OperationQueue()
+        queue.name = "MotionManagerQueue"
+        queue.maxConcurrentOperationCount = 1
+        return queue
+    }()
+
+    func start() {
+        guard !motionManager.isDeviceMotionActive else { return }
+
+        motionManager.startDeviceMotionUpdates(to: queue) { motion, error in
+            if let error = error { print("CMMotionManager: \(error)") }
+            guard let motion = motion else { return }
+
+            print(motion)
+        }
+    }
+
+    func stop() {
+        motionManager.stopDeviceMotionUpdates()
+    }
 }
