ad36df97fc502b7005c3ea15259ceb6972cad1d0
brien84 <houseinblues@gmail.com>
2021-04-02 19:10:09 +0300

    Create `PrimaryControls` view

---
+++ BilliardsTracker WatchKit Extension/Views/PrimaryControls.swift
@@ -0,0 +1,20 @@
+//
+//  PrimaryControls.swift
+//  BilliardsTracker WatchKit Extension
+//
+//  Created by Marius on 2021-04-02.
+//
+
+import SwiftUI
+
+struct PrimaryControls: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PrimaryControls_Previews: PreviewProvider {
+    static var previews: some View {
+        PrimaryControls()
+    }
+}
