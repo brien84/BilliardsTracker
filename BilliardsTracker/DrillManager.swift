aece43eef5402b5267c6632b717221a96be3df00
brien84 <houseinblues@gmail.com>
2021-04-04 19:34:58 +0300

    Conform to `WCSessionDelegate` and implement required methods

--- BilliardsTracker/DrillManager.swift
+++ BilliardsTracker/DrillManager.swift
@@ -6,7 +6,31 @@
 //
 
 import Foundation
+import WatchConnectivity
 
-final class DrillManager: ObservableObject {
+final class DrillManager: NSObject, ObservableObject {
+    private let session = WCSession.default
 
+    override init() {
+        super.init()
+
+        session.delegate = self
+        session.activate()
+    }
+
+}
+
+extension DrillManager: WCSessionDelegate {
+    func session(_ session: WCSession, activationDidCompleteWith activationState: WCSessionActivationState, error: Error?) {
+        print("activationDidCompleteWith: \(activationState.rawValue)")
+        if let error = error { print(error) }
+    }
+
+    func sessionDidBecomeInactive(_ session: WCSession) {
+        print("sessionDidBecomeInactive")
+    }
+
+    func sessionDidDeactivate(_ session: WCSession) {
+        print("sessionDidDeactivate")
+    }
 }
