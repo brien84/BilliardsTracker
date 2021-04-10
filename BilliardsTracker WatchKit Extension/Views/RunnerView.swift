3de6bc83b575111ec139f4d7fb536fef6749a6b1
brien84 <houseinblues@gmail.com>
2021-04-10 21:14:19 +0300

    Create `RunnerView`

---
+++ BilliardsTracker WatchKit Extension/Views/RunnerView.swift
@@ -0,0 +1,55 @@
+//
+//  RunnerView.swift
+//  BilliardsTracker WatchKit Extension
+//
+//  Created by Marius on 2021-04-10.
+//
+
+import SwiftUI
+
+
+struct RunnerView: View {
+    @EnvironmentObject var runner: DrillRunner
+
+    private var mode: Mode
+
+    @State private var currentTab: Int = 0
+
+    init(_ mode: Mode) {
+        self.mode = mode
+    }
+
+    var body: some View {
+        Group {
+            if runner.isActive {
+                Group {
+                    if runner.isCompleted {
+                        CompletionView()
+                    } else {
+                        TabView(selection: $currentTab) {
+                            PrimaryControls().tag(0)
+                            SecondaryControls().tag(1)
+                        }
+                    }
+                }
+                .navigationBarBackButtonHidden(true)
+            } else {
+                StartView()
+                    .navigationBarBackButtonHidden(false)
+            }
+        }
+        .onAppear {
+            runner.mode = mode
+        }
+        .onDisappear {
+            runner.mode = nil
+        }
+    }
+}
+
+struct RunnerView_Previews: PreviewProvider {
+    static var previews: some View {
+        RunnerView(.paired)
+            .environmentObject(DrillRunner())
+    }
+}
