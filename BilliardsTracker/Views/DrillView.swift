27f66e1fedab4db8afaea45733f94552fcababf8
brien84 <houseinblues@gmail.com>
2021-04-19 00:11:40 +0300

    Create `DrillView`

---
+++ BilliardsTracker/Views/DrillView.swift
@@ -0,0 +1,20 @@
+//
+//  DrillView.swift
+//  BilliardsTracker
+//
+//  Created by Marius on 2021-04-19.
+//
+
+import SwiftUI
+
+struct DrillView: View {
+    var body: some View {
+        Text("Drill View")
+    }
+}
+
+struct DrillView_Previews: PreviewProvider {
+    static var previews: some View {
+        DrillView()
+    }
+}
