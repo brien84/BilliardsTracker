c123e87b79e4a78b2cca0265324d9400a5b3fd18
brien84 <houseinblues@gmail.com>
2021-04-19 23:13:21 +0300

    Add alerts

--- BilliardsTracker/Views/DrillView.swift
+++ BilliardsTracker/Views/DrillView.swift
@@ -54,6 +54,18 @@ struct DrillView: View {
             }
 
         }
+        .alert(item: $manager.connectivityError) { status in
+            switch status {
+            case .notReady:
+                return Alert(title: Text("Watch app is not in Tracked mode!"),
+                      message: Text("Make sure Tracked mode is selected in Watch app."),
+                      dismissButton: .default(Text("OK")))
+            case .notReachable:
+                return Alert(title: Text("Watch app is not reachable!"),
+                      message: Text("Make sure BilliardsTracker Watch app is installed and running."),
+                      dismissButton: .default(Text("OK")))
+            }
+        }
         .disabled(manager.runState == .loading)
         .overlay(manager.runState == .loading ? AnyView(loadingView) : AnyView(EmptyView()))
     }
