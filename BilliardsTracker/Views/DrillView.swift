d0f5e06e586db04af47e4eb04f9e05b71e25dc47
brien84 <houseinblues@gmail.com>
2021-04-19 22:50:28 +0300

    Add plain `Drill` representation UI

--- BilliardsTracker/Views/DrillView.swift
+++ BilliardsTracker/Views/DrillView.swift
@@ -8,13 +8,38 @@
 import SwiftUI
 
 struct DrillView: View {
+    @EnvironmentObject var manager: DrillManager
+    private let drill: Drill
+
+    init(drill: Drill) {
+        self.drill = drill
+    }
+
     var body: some View {
-        Text("Drill View")
+        Button {
+            manager.start(drill: drill)
+        } label: {
+            VStack(spacing: 16) {
+                Text(drill.title)
+                    .font(.title)
+                HStack {
+                    Image(systemName: "arrow.left.arrow.right")
+                        .imageScale(.small)
+                    Text(String(drill.attempts))
+                }
+            }
+            .padding()
+            .frame(maxWidth: .infinity)
+        }
     }
 }
 
 struct DrillView_Previews: PreviewProvider {
+    static var manager = DrillManager(store: DrillStore(inMemory: true))
+    static var drill = manager.drills.first!
+
     static var previews: some View {
-        DrillView()
+        DrillView(drill: drill)
+            .environmentObject(manager)
     }
 }
