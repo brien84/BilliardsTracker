31f1c5dab9f5deee7bd4e6e502482ef4588cb03a
brien84 <houseinblues@gmail.com>
2021-04-22 21:08:02 +0300

    Create `StatisticsManager`

---
+++ BilliardsTracker/StatisticsManager.swift
@@ -0,0 +1,16 @@
+//
+//  StatisticsManager.swift
+//  BilliardsTracker
+//
+//  Created by Marius on 2021-04-22.
+//
+
+import Foundation
+
+struct StatisticsManager {
+    private let drill: Drill
+
+    init(drill: Drill) {
+        self.drill = drill
+    }
+}
