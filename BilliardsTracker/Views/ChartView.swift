29e9dbfa82ba36caf4e15b95182d2c090ed125a7
brien84 <houseinblues@gmail.com>
2021-05-03 21:47:48 +0300

    Create `ChartView`

---
+++ BilliardsTracker/Views/ChartView.swift
@@ -0,0 +1,20 @@
+//
+//  ChartView.swift
+//  BilliardsTracker
+//
+//  Created by Marius on 2021-05-03.
+//
+
+import SwiftUI
+
+struct ChartView: View {
+    var body: some View {
+        Text("ChartView")
+    }
+}
+
+struct ChartView_Previews: PreviewProvider {
+    static var previews: some View {
+        ChartView()
+    }
+}
