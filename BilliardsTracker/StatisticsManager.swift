bb00e099b1d5c9746790c35cae9c8ff01bb81092
brien84 <houseinblues@gmail.com>
2021-05-05 18:50:27 +0300

    Add `totalAttempts`, `totalPotCount`, `totalMissCount`, `pottingPercentage` computed properties

--- BilliardsTracker/StatisticsManager.swift
+++ BilliardsTracker/StatisticsManager.swift
@@ -9,6 +9,28 @@ import Foundation
 
 struct StatisticsManager {
     private let drill: Drill
+    
+    var results: [DrillResult] {
+        drill.results
+    }
+
+    var totalAttempts: Int {
+        results.reduce(0, { $0 + $1.potCount + $1.missCount })
+    }
+
+    var totalPotCount: Int {
+        results.reduce(0, { $0 + $1.potCount })
+    }
+
+    var totalMissCount: Int {
+        results.reduce(0, { $0 + $1.missCount })
+    }
+
+    var pottingPercentage: Int {
+        guard totalAttempts > 0 else { return 0 }
+
+        return Int(Double(totalPotCount) / Double(totalAttempts) * 100)
+    }
 
     init(drill: Drill) {
         self.drill = drill
