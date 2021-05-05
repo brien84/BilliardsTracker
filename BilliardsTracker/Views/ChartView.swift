65d0d10e18d4a631a83364540b28f59a4953299a
brien84 <houseinblues@gmail.com>
2021-05-05 18:26:43 +0300

    Add chart labels

--- BilliardsTracker/Views/ChartView.swift
+++ BilliardsTracker/Views/ChartView.swift
@@ -8,13 +8,93 @@
 import SwiftUI
 
 struct ChartView: View {
+    private let maxValue: Int
+    private let results: [DrillResult]
+
+    private let labelHeight = UIFont.preferredFont(forTextStyle: .caption1).lineHeight
+
+    @State private var isReady = false
+
     var body: some View {
-        Text("ChartView")
+        ZStack {
+            GeometryReader { proxy in
+                ForEach(isReady ? calculateLabelValues(in: proxy.size) : [], id: \.self) { i in
+                    Label(value: i)
+                        .offset(x: 0, y: proxy.size.height * (1 - CGFloat(i) / CGFloat(maxValue)))
+                        .offset(x: 0, y: -labelHeight / 2)
+                }
+            }
+        }
+        .onAppear {
+            isReady = true
+        }
+    }
+
+    init(results: [DrillResult], maxValue: Int) {
+        self.results = results
+        self.maxValue = maxValue
+    }
+
+    private func calculateLabelValues(in size: CGSize) -> [Int] {
+        let maxLabelCount = Int(size.height / (2 * labelHeight))
+
+        let values = Array(0...maxValue)
+
+        if maxValue <= maxLabelCount {
+            return values
+        } else {
+            return reduce(labelValues: values, maxLabelCount: maxLabelCount)
+        }
+    }
+
+    private func reduce(labelValues values: [Int], maxLabelCount: Int) -> [Int] {
+        let reduced = stride(from: 0, to: values.count, by: 2).map { i in
+            // Reverse `values` to ensure highest value is not removed.
+            values.reversed()[i]
+        }
+
+        if reduced.count <= maxLabelCount {
+            return reduced.reversed()
+        } else {
+            return reduce(labelValues: reduced.reversed(), maxLabelCount: maxLabelCount)
+        }
     }
 }
 
 struct ChartView_Previews: PreviewProvider {
+    static var manager = DrillManager(store: DrillStore(inMemory: true))
+    static var drill = manager.drills.first!
+
     static var previews: some View {
-        ChartView()
+        ChartView(results: drill.results, maxValue: drill.attempts)
+    }
+}
+
+private struct Label: View {
+    var value: Int
+
+    var body: some View {
+        HStack(alignment: .center, spacing: 10) {
+            ZStack {
+                Text("\(value)")
+                Text("100")
+                    .hidden()
+            }
+            .font(Font.caption.bold())
+
+            LabelLine()
+                .stroke(Color.gray, style: StrokeStyle(lineWidth: 1, lineCap: .round, lineJoin: .round, dash: [5, 10]))
+                .frame(height: 1)
+                .offset(x: 0, y: 0.5)
+        }
+    }
+}
+
+struct LabelLine: Shape {
+    func path(in rect: CGRect) -> Path {
+        Path { p in
+            p.move(to: CGPoint(x: 0, y: 0))
+            p.addLine(to: CGPoint(x: rect.maxX, y: 0))
+        }
     }
 }
