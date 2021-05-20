ccb964de25a9a36e63c92b9e31a7885249cb4d62
brien84 <houseinblues@gmail.com>
2021-05-20 21:47:09 +0300

    Add test cases

--- BilliardsTrackerTests/StatisticsManagerTests.swift
+++ BilliardsTrackerTests/StatisticsManagerTests.swift
@@ -21,5 +21,82 @@ final class StatisticsManagerTests: XCTestCase {
         sut = nil
     }
 
+    func testAfterDateNilCountsAllResults() throws {
+        let testDate = Date()
 
+        let drill = Drill(context: store.persistentContainer.viewContext)
+
+        let result0 = DrillResult(context: store.persistentContainer.viewContext)
+        result0.date = Date(timeInterval: -3600, since: testDate)
+        result0.drill = drill
+
+        let result1 = DrillResult(context: store.persistentContainer.viewContext)
+        result1.date = Date(timeInterval: 3600, since: testDate)
+        result1.drill = drill
+
+        sut = StatisticsManager(drill: drill)
+
+        XCTAssertEqual(sut.results.count, 2)
+    }
+
+    func testResultsAreCountedOnlyAfterDate() throws {
+        let testDate = Date()
+
+        let drill = Drill(context: store.persistentContainer.viewContext)
+
+        let result0 = DrillResult(context: store.persistentContainer.viewContext)
+        result0.date = Date(timeInterval: -3600, since: testDate)
+        result0.drill = drill
+
+        let result1 = DrillResult(context: store.persistentContainer.viewContext)
+        result1.date = Date(timeInterval: 3600, since: testDate)
+        result1.drill = drill
+
+        sut = StatisticsManager(drill: drill, afterDate: testDate)
+
+        XCTAssertEqual(sut.results.count, 1)
+    }
+
+    func testStatisticsCalculation() throws {
+        let drill = Drill(context: store.persistentContainer.viewContext)
+        drill.attempts = 10
+
+        let result0 = DrillResult(context: store.persistentContainer.viewContext)
+        result0.potCount = 5
+        result0.missCount = 5
+        result0.drill = drill
+
+        let result1 = DrillResult(context: store.persistentContainer.viewContext)
+        result1.potCount = 5
+        result1.missCount = 5
+        result1.drill = drill
+
+        sut = StatisticsManager(drill: drill)
+
+        XCTAssertEqual(sut.totalAttempts, 20)
+        XCTAssertEqual(sut.totalPotCount, 10)
+        XCTAssertEqual(sut.totalMissCount, 10)
+        XCTAssertEqual(sut.totalPottingPercentage, 50)
+    }
+
+    func testChartDataPoints() throws {
+        let drill = Drill(context: store.persistentContainer.viewContext)
+        drill.attempts = 10
+
+        let result0 = DrillResult(context: store.persistentContainer.viewContext)
+        result0.date = Date(timeIntervalSinceReferenceDate: 1)
+        result0.potCount = 8
+        result0.missCount = 2
+        result0.drill = drill
+
+        let result1 = DrillResult(context: store.persistentContainer.viewContext)
+        result1.date = Date(timeIntervalSinceReferenceDate: 50)
+        result1.potCount = 2
+        result1.missCount = 8
+        result1.drill = drill
+
+        sut = StatisticsManager(drill: drill)
+
+        XCTAssertEqual(sut.chartDataPoints, [0.8, 0.2])
+    }
 }
