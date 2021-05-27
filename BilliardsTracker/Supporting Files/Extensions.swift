d642a95a6bb97ce87e40df0c5e14f5f8cc948855
brien84 <houseinblues@gmail.com>
2021-05-27 22:44:41 +0300

    Add `fixFlickering()` `ScrollView` extension

--- BilliardsTracker/Supporting Files/Extensions.swift
+++ BilliardsTracker/Supporting Files/Extensions.swift
@@ -55,3 +55,33 @@ extension Date {
         return formatter.string(from: self)
     }
 }
+
+extension ScrollView {
+    /// Fixes navigation bar title flicker while scrolling.
+    func fixFlickering() -> some View {
+        self.fixFlickering { scrollView in
+            scrollView
+        }
+    }
+
+    func fixFlickering<T: View>(@ViewBuilder configurator: @escaping (ScrollView<AnyView>) -> T) -> some View {
+        GeometryReader { geometryWithSafeArea in
+            GeometryReader { geometry in
+                configurator(
+                    ScrollView<AnyView>(self.axes, showsIndicators: self.showsIndicators) {
+                        AnyView(
+                            VStack {
+                                self.content
+                            }
+                            .padding(.top, geometryWithSafeArea.safeAreaInsets.top)
+                            .padding(.bottom, geometryWithSafeArea.safeAreaInsets.bottom)
+                            .padding(.leading, geometryWithSafeArea.safeAreaInsets.leading)
+                            .padding(.trailing, geometryWithSafeArea.safeAreaInsets.trailing)
+                        )
+                    }
+                )
+            }
+            .edgesIgnoringSafeArea(.all)
+        }
+    }
+}
