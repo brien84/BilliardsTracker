0a1907d912000eb623189902a2ee8ff6ed8f6e71
brien84 <houseinblues@gmail.com>
2021-05-27 20:50:36 +0300

    Rework UI

--- BilliardsTracker/Views/DrillView.swift
+++ BilliardsTracker/Views/DrillView.swift
@@ -25,88 +25,58 @@ struct DrillView: View {
             NavigationLink(destination: RunningView(drill: drill, startDate: manager.startDate),
                            isActive: navigationBinding) { EmptyView() }.disabled(true)
 
-            Color(red: 0/255.0, green: 184/255.0, blue: 148/255.0)
-                .clipShape(RoundedRectangle(cornerRadius: 25.0))
+            Color.secondaryBackground
 
-            VStack {
-                Text(drill.title)
-                    .foregroundColor(.white)
-                    .font(Font.largeTitle.weight(.semibold))
-                    .frame(maxWidth: .infinity, alignment: .leading)
-                    .padding(.top)
-                    .fixedSize(horizontal: false, vertical: true)
-
-                HStack {
-                    Image(systemName: "arrow.left.arrow.right")
-                        .imageScale(.medium)
+            HStack(spacing: 0) {
+                ZStack {
+                    Text("100").opacity(0)
                     Text("\(drill.attempts)")
                 }
-                .frame(maxWidth: .infinity, alignment: .center)
-                .foregroundColor(.white)
-                .padding(4)
+                .frame(maxHeight: .infinity)
+                .padding(.horizontal)
+                .font(Font.title.weight(.semibold))
+                .foregroundColor(.primaryText)
 
-                Image(systemName: "xmark.seal")
-                    .font(Font.title3.weight(.black))
-                    .foregroundColor(.red)
-                    .isHidden(!drill.isFailable)
+                Text(drill.title.uppercased())
+                    .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topLeading)
+                    .padding()
+                    .font(Font.headline.weight(.bold))
+                    .foregroundColor(.secondaryText)
 
-                HStack {
-                    deleteButton
+                VStack(spacing: 4) {
+                    failableIcon
+                        .frame(maxHeight: .infinity, alignment: .top)
+                        .foregroundColor(.customRed)
+                        .opacity(drill.isFailable ? 1 : 0)
+
                     Spacer()
-                    statisticsButton
-                }
-                .padding(.top)
-                .foregroundColor(.white)
-            }
-            .padding()
 
-        }.onTapGesture {
+                    statisticsButton
+                        .frame(maxHeight: .infinity, alignment: .bottom)
+                        .foregroundColor(.secondaryText)
+                }
+                .padding()
+            }
+        }
+        .cornerRadius(10)
+        .onTapGesture {
             manager.start(drill: drill)
         }
     }
 
-    @State private var shouldDelete = false
-
-    private var deleteButton: some View {
-        Button {
-            shouldDelete = true
-        } label: {
-            Image(systemName: "trash")
-                .imageScale(.large)
-        }
-        .alert(isPresented: $shouldDelete, content: {
-            Alert(
-                title: Text("Confirmation"),
-                message: Text("Are you sure you want to delete this drill?"),
-                primaryButton: .destructive(Text("Delete")) {
-                    manager.delete(drill: drill)
-                },
-                secondaryButton: .cancel()
-            )
-        })
+    private var failableIcon: some View {
+        Image(systemName: "xmark.seal")
+            .font(Font.title3.weight(.regular))
     }
 
     private var statisticsButton: some View {
         NavigationLink(
             destination: StatisticsView(drill: drill),
             label: {
-                Image(systemName: "chart.bar.doc.horizontal")
+                Image(systemName: "chart.bar.xaxis")
                     .imageScale(.large)
             }
         )
-    }
-
-}
-
-extension View {
-    /// Hide or show the view based on a boolean value.
-    @ViewBuilder
-    func isHidden(_ hidden: Bool) -> some View {
-        if hidden {
-            self.hidden()
-        } else {
-            self
-        }
     }
 }
 
@@ -114,10 +84,20 @@ struct DrillView_Previews: PreviewProvider {
     static var manager = DrillManager(store: try! DrillStore(inMemory: true, isPreview: true))
     static var drill = manager.drills.first!
 
+    static var view: some View {
+        ZStack {
+            Color.primaryBackground
+                .ignoresSafeArea()
+
+            DrillView(drill: drill)
+                .environmentObject(manager)
+                .fixedSize(horizontal: false, vertical: true)
+                .padding()
+        }
+    }
+
     static var previews: some View {
-        DrillView(drill: drill)
-            .environmentObject(manager)
-            .aspectRatio(2.0, contentMode: .fit)
-            .padding()
+        view.preferredColorScheme(.light)
+        view.preferredColorScheme(.dark)
     }
 }
