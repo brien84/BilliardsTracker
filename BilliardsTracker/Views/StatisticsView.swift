009bd327c8d699f9dbab33dbdf4c480ca164dd6f
brien84 <houseinblues@gmail.com>
2021-05-28 14:00:45 +0300

    Add delete navigationBar button

--- BilliardsTracker/Views/StatisticsView.swift
+++ BilliardsTracker/Views/StatisticsView.swift
@@ -8,9 +8,8 @@
 import SwiftUI
 
 struct StatisticsView: View {
+    @EnvironmentObject var manager: DrillManager
     private let statistics: StatisticsManager
-
-    @State private var showHistory = false
 
     init(drill: Drill) {
         self.statistics = StatisticsManager(drill: drill)
@@ -36,15 +35,43 @@ struct StatisticsView: View {
                     .background(Color.yellow)
             }
         }
-        .navigationBarItems(trailing: toggleHistoryButton)
         .navigationBarTitle(statistics.drill.title)
+        .navigationBarItems(trailing: HStack(alignment: .firstTextBaseline, spacing: 30) {
+                                deleteButton; toggleHistoryButton })
     }
+
+    @State private var showDeleteAlert = false
+
+    private var deleteButton: some View {
+        Button {
+            showDeleteAlert = true
+        } label: {
+            Image(systemName: "trash")
+                .font(Font.body)
+                .imageScale(.large)
+                .foregroundColor(.customRed)
+        }
+        .alert(isPresented: $showDeleteAlert, content: {
+            Alert(
+                title: Text("Confirmation"),
+                message: Text("Are you sure you want to delete this drill?"),
+                primaryButton: .destructive(Text("Delete")) {
+                    manager.delete(drill: statistics.drill)
+                },
+                secondaryButton: .cancel()
+            )
+        })
+    }
+
+    @State private var showHistory = false
 
     private var toggleHistoryButton: some View {
         Button {
             showHistory.toggle()
         } label: {
-            Text(showHistory ? "Chart" : "History")
+            Image(systemName: showHistory ? "chart.bar.xaxis" : "tray.full")
+                .font(Font.body)
+                .imageScale(.large)
         }
     }
 }
@@ -124,9 +151,15 @@ struct StatisticsView_Previews: PreviewProvider {
     static var manager = DrillManager(store: try! DrillStore(inMemory: true, isPreview: true))
     static var drill = manager.drills.first!
 
-    static var previews: some View {
+    static var view: some View {
         NavigationView {
             StatisticsView(drill: drill)
+                .environmentObject(manager)
         }
+    }
+
+    static var previews: some View {
+        view.preferredColorScheme(.light)
+        view.preferredColorScheme(.dark)
     }
 }
