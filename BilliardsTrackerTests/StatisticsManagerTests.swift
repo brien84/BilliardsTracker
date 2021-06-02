a816efeecb8dcd02d46cc779cddc37fa4f1e142c
brien84 <houseinblues@gmail.com>
2021-06-02 22:40:47 +0300

    Add `testFailableStatisticsCalculation()` test case

--- BilliardsTrackerTests/StatisticsManagerTests.swift
+++ BilliardsTrackerTests/StatisticsManagerTests.swift
@@ -21,18 +21,6 @@ final class StatisticsManagerTests: XCTestCase {
         sut = nil
     }
 
-    func testAfterDateNilCountsAllResults() throws {
-        let testDate = Date()
-
-        let drill = createDrill(attempts: 1)
-        store.addResult(from: createResultContext(date: Date(timeInterval: -3600, since: testDate)), to: drill)
-        store.addResult(from: createResultContext(date: Date(timeInterval: 3600, since: testDate)), to: drill)
-
-        sut = StatisticsManager(drill: drill)
-
-        XCTAssertEqual(sut.results.count, 2)
-    }
-
     func testResultsAreCountedOnlyAfterDate() throws {
         let testDate = Date()
 
@@ -45,6 +33,18 @@ final class StatisticsManagerTests: XCTestCase {
         XCTAssertEqual(sut.results.count, 1)
     }
 
+    func testAllResultsAreCountedWhenAfterDateIsNil() throws {
+        let testDate = Date()
+
+        let drill = createDrill(attempts: 1)
+        store.addResult(from: createResultContext(date: Date(timeInterval: -3600, since: testDate)), to: drill)
+        store.addResult(from: createResultContext(date: Date(timeInterval: 3600, since: testDate)), to: drill)
+
+        sut = StatisticsManager(drill: drill)
+
+        XCTAssertEqual(sut.results.count, 2)
+    }
+
     func testStatisticsCalculation() throws {
         let drill = createDrill(attempts: 10)
         store.addResult(from: createResultContext(potCount: 5, missCount: 5), to: drill)
@@ -52,15 +52,27 @@ final class StatisticsManagerTests: XCTestCase {
 
         sut = StatisticsManager(drill: drill)
 
-        XCTAssertEqual(sut.totalAttempts, 20)
-        XCTAssertEqual(sut.totalPotCount, 10)
-        XCTAssertEqual(sut.totalMissCount, 10)
-        XCTAssertEqual(sut.totalPottingPercentage, 50)
+        XCTAssertEqual(sut.attemptsCount, 20)
+        XCTAssertEqual(sut.potCount, 10)
+        XCTAssertEqual(sut.missCount, 10)
+        XCTAssertEqual(sut.pottingPercentage, 50)
+    }
+
+    func testFailableStatisticsCalculation() throws {
+        let drill = createDrill(attempts: 10, isFailable: true)
+        store.addResult(from: createResultContext(potCount: 0, missCount: 1), to: drill)
+        store.addResult(from: createResultContext(potCount: 4, missCount: 1), to: drill)
+        store.addResult(from: createResultContext(potCount: 10, missCount: 0), to: drill)
+
+        sut = StatisticsManager(drill: drill)
+
+        XCTAssertEqual(sut.failableCompletedCount, 1)
+        XCTAssertEqual(sut.failableCompletionPercentage, 33)
+        XCTAssertEqual(sut.averagePots, 4.6)
     }
 
     func testChartDataPoints() throws {
         let drill = createDrill(attempts: 10)
-
         store.addResult(from: createResultContext(potCount: 8, missCount: 2), to: drill)
         store.addResult(from: createResultContext(potCount: 2, missCount: 8), to: drill)
 
@@ -71,8 +83,8 @@ final class StatisticsManagerTests: XCTestCase {
 
     // MARK: - Helpers
 
-    private func createDrill(attempts: Int) -> Drill {
-        store.createDrill(title: "", attempts: attempts, isFailable: false)
+    private func createDrill(attempts: Int, isFailable: Bool = false) -> Drill {
+        store.createDrill(title: "", attempts: attempts, isFailable: isFailable)
         return store.loadDrills().first!
     }
 
