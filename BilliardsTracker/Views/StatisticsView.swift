6938edc00aa9842a8810a9298f956122db792f39
brien84 <houseinblues@gmail.com>
2021-06-07 17:39:09 +0300

    Update background color

--- BilliardsTracker/Views/StatisticsView.swift
+++ BilliardsTracker/Views/StatisticsView.swift
@@ -16,23 +16,22 @@ struct StatisticsView: View {
     }
 
     var body: some View {
-        VStack(spacing: 0) {
-            StatisticsPanel(statistics: statistics)
+        ZStack {
+            Color.primaryBackground
+                .ignoresSafeArea()
 
-            Text(showHistory ? "History" : "Last results")
-                .font(.title)
-                .bold()
-                .padding()
-                .frame(maxWidth: .infinity, alignment: .leading)
+            VStack(spacing: .zero) {
+                StatisticsPanel(statistics: statistics)
 
-            Divider()
-
-            if showHistory {
-                HistoryView(results: statistics.results)
-            } else {
-                ChartView(dataPoints: statistics.chartDataPoints, maxValue: statistics.drill.attempts)
-                    .padding()
-                    .background(Color.yellow)
+                CardView {
+                    if showHistory {
+                        ResultsView(results: statistics.results)
+                    } else {
+                        ChartView(dataPoints: statistics.chartDataPoints, maxValue: statistics.drill.attempts)
+                            .padding()
+                    }
+                }
+                .setTitle(showHistory ? "History" : "Performance")
             }
         }
         .navigationBarTitle(statistics.drill.title)
@@ -76,80 +75,31 @@ struct StatisticsView: View {
     }
 }
 
-private struct HistoryView: View {
-    private let results: [DrillResult]
-
-    init(results: [DrillResult]) {
-        self.results = results
-    }
-
-    var body: some View {
-        ScrollView {
-            ForEach(results) { result in
-                ResultView(result: result)
-            }
-        }
-    }
-}
-
-private struct StatisticsPanel: View {
-    private let statistics: StatisticsManager
-
-    init(statistics: StatisticsManager) {
-        self.statistics = statistics
-    }
-
-    var body: some View {
-        VStack(spacing: 0) {
-
-            HStack {
-                Text("\(statistics.results.count) sessions")
-                    .font(.callout)
-                    .fontWeight(.light)
-                Spacer()
-                Text("\(statistics.totalAttempts) attempts")
-                    .font(.callout)
-                    .fontWeight(.light)
-
-            }
-            .frame(maxWidth: .infinity, alignment: .leading)
-            .padding()
-            .background(Color.red)
-
-            HStack(spacing: 0) {
-                VStack {
-                    Text("\(statistics.totalPotCount)")
-                        .font(.title2)
-                    Text("Pots")
-                        .font(.body)
-                }
-                .frame(maxWidth: .infinity)
-
-                VStack {
-                    Text("\(statistics.totalMissCount)")
-                        .font(.title2)
-                    Text("Misses")
-                        .font(.body)
-                }
-                .frame(maxWidth: .infinity)
-
-                VStack {
-                    Text("\(statistics.totalPottingPercentage)%")
-                        .font(.title2)
-                    Text("Average")
-                        .font(.body)
-                }
-                .frame(maxWidth: .infinity)
-            }
-            .padding()
-            .background(Color.blue)
-        }
-    }
-}
-
 struct StatisticsView_Previews: PreviewProvider {
     static var manager = DrillManager(store: try! DrillStore(inMemory: true, isPreview: true))
     static var drill = manager.drills.first!
+
+    static var view: some View {
+        NavigationView {
+            StatisticsView(drill: drill)
+                .environmentObject(manager)
+        }
+    }
+
+    static var previews: some View {
+        view.preferredColorScheme(.light)
+        view.preferredColorScheme(.dark)
+    }
+}
+
+struct StatisticsViewNotEnoughData_Previews: PreviewProvider {
+    static var store = try! DrillStore(inMemory: true, isPreview: false)
+    static var manager = DrillManager(store: store)
+
+    static var drill: Drill = {
+        store.createDrill(title: "EmptyDrill", attempts: 10, isFailable: false)
+        return manager.drills.first!
+    }()
 
     static var view: some View {
         NavigationView {
