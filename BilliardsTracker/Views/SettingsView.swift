e39beefc5863981efe031fed6d63191cdddb8f43
brien84 <houseinblues@gmail.com>
2021-06-14 13:19:30 +0300

    Create `SettingsView`

---
+++ BilliardsTracker/Views/SettingsView.swift
@@ -0,0 +1,45 @@
+//
+//  SettingsView.swift
+//  BilliardsTracker
+//
+//  Created by Marius on 2021-06-13.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+
+    var body: some View {
+        GeometryReader { proxy in
+            ZStack {
+                Color.primaryBackground
+                    .edgesIgnoringSafeArea(.bottom)
+
+            }
+            .frame(width: proxy.size.width * .widthModifier)
+        }
+    }
+
+}
+
+private extension CGFloat {
+    static var widthModifier: CGFloat {
+        0.75
+    }
+}
+
+struct SettingsView_Previews: PreviewProvider {
+    static var view: some View {
+        ZStack {
+            Color.secondaryBackground
+                .ignoresSafeArea()
+
+            SettingsView()
+        }
+    }
+
+    static var previews: some View {
+        view.preferredColorScheme(.light)
+        view.preferredColorScheme(.dark)
+    }
+}
