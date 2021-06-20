36ce85db7c1164ac07fdc0e6b53e2abb714c3b2c
brien84 <houseinblues@gmail.com>
2021-06-20 22:15:35 +0300

    Add `Color` extensions

--- BilliardsTracker WatchKit Extension/Supporting FIles/Extensions.swift
+++ BilliardsTracker WatchKit Extension/Supporting FIles/Extensions.swift
@@ -6,3 +6,29 @@
 //
 
 import SwiftUI
+
+extension Color {
+    static var primaryElement: Color {
+        Color("primaryElement")
+    }
+
+    static var secondaryElement: Color {
+        Color("secondaryElement")
+    }
+
+    static var customGreen: Color {
+        Color("customGreen")
+    }
+
+    static var customRed: Color {
+        Color("customRed")
+    }
+
+    static var customYellow: Color {
+        Color("customYellow")
+    }
+
+    static var customBlue: Color {
+        Color("customBlue")
+    }
+}
