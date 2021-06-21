b119cd7870e10dd065b6c017e9533377a7a5cdef
brien84 <houseinblues@gmail.com>
2021-06-21 20:24:29 +0300

    Add `currentTab` binding

--- BilliardsTracker WatchKit Extension/Views/SecondaryControls.swift
+++ BilliardsTracker WatchKit Extension/Views/SecondaryControls.swift
@@ -8,33 +8,38 @@
 import SwiftUI
 
 struct SecondaryControls: View {
-    @EnvironmentObject var runner: SessionManager
+    @EnvironmentObject var session: SessionManager
+
+    @Binding var currentTab: Int
 
     var body: some View {
         VStack {
             HStack {
                 SecondaryControlButton(title: "Stop", imageName: "multiply", color: .customRed) {
-                    runner.isActive = false
+                    session.isActive = false
+                    currentTab = 0
                 }
 
                 SecondaryControlButton(title: "Undo", imageName: "arrow.uturn.backward", color: .customBlue) {
-                    runner.undo()
+                    session.undo()
+                    currentTab = 0
                 }
-                .disabled(runner.didPotLastAttempt == nil)
+                .disabled(session.didPotLastAttempt == nil)
             }
 
             HStack {
                 SecondaryControlButton(title: "Pause", imageName: "pause", color: .customYellow) {
-                    runner.isPaused = true
+                    session.isPaused = true
+                    currentTab = 0
                 }
-                .disabled(runner.isPaused)
+                .disabled(session.isPaused)
 
                 SecondaryControlButton(title: "Resume", imageName: "play", color: .customGreen) {
-                    runner.isPaused = false
+                    session.isPaused = false
+                    currentTab = 0
                 }
-                .disabled(!runner.isPaused)
+                .disabled(!session.isPaused)
             }
-            
         }
     }
 }
@@ -57,12 +62,13 @@ private struct SecondaryControlButton: View {
     var body: some View {
         VStack {
             Button {
-                action()
+                withAnimation() { action() }
             } label: {
                 Image(systemName: imageName)
                     .frame(width: 25, height: 25)
                     .font(Font.title2.weight(.semibold))
                     .foregroundColor(color)
+                    .padding()
             }
             .buttonStyle(BorderedButtonStyle(tint: color))
 
@@ -75,7 +81,7 @@ private struct SecondaryControlButton: View {
 
 struct SecondaryControls_Previews: PreviewProvider {
     static var previews: some View {
-        SecondaryControls()
+        SecondaryControls(currentTab: .constant(1))
             .environmentObject(SessionManager())
     }
 }
