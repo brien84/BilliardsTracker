81e54c519ab12d08041ff539e8b7af675d09e732
brien84 <houseinblues@gmail.com>
2021-06-29 23:53:41 +0300

    Implement `OnboardView`

--- BilliardsTracker WatchKit Extension/BilliardsTrackerApp.swift
+++ BilliardsTracker WatchKit Extension/BilliardsTrackerApp.swift
@@ -11,11 +11,35 @@ import SwiftUI
 struct BilliardsTrackerApp: App {
     @ObservedObject var runner = SessionManager()
 
+    @State private var showOnboard = false
+
     var body: some Scene {
         WindowGroup {
             NavigationView {
-                MenuView()
-            }.environmentObject(runner)
+                ZStack {
+                    NavigationLink(
+                        destination: OnboardView(),
+                        isActive: $showOnboard,
+                        label: {
+                            Color.clear
+                        })
+
+                    MenuView()
+                }
+            }
+            .environmentObject(runner)
+            .onAppear {
+                if UserDefaults.standard.object(forKey: .userDefaultsOnboardKey) == nil {
+                    showOnboard = true
+                    UserDefaults.standard.set(true, forKey: .userDefaultsOnboardKey)
+                }
+            }
         }
+    }
+}
+
+private extension String {
+    static var userDefaultsOnboardKey: String {
+        "userDefaultsOnboardKey"
     }
 }
