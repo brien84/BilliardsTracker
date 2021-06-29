2bbfdfa8f0e79af2459a66afa26ff2bdfc23500a
brien84 <houseinblues@gmail.com>
2021-06-29 17:43:53 +0300

    Create `OnboardView`

---
+++ BilliardsTracker WatchKit Extension/Views/OnboardView.swift
@@ -0,0 +1,20 @@
+//
+//  OnboardView.swift
+//  BilliardsTracker WatchKit Extension
+//
+//  Created by Marius on 2021-06-29.
+//
+
+import SwiftUI
+
+struct OnboardView: View {
+    var body: some View {
+        Text("Onboard")
+    }
+}
+
+struct OnboardView_Previews: PreviewProvider {
+    static var previews: some View {
+        OnboardView()
+    }
+}
