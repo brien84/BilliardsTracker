6aa48bbc94f58ad9cfe9d186eb6e84a87ed1a5ec
brien84 <houseinblues@gmail.com>
2021-07-06 00:30:57 +0300

    Add `addDrill(title:, attempts:, isFailable:)` method

--- BilliardsTracker/Managers/StoreManager.swift
+++ BilliardsTracker/Managers/StoreManager.swift
@@ -9,5 +9,17 @@ import Combine
 import Foundation
 
 final class StoreManager: ObservableObject {
+    private let store: DrillStore
 
+    @Published var drills = [Drill]()
+
+    init(store: DrillStore) {
+        self.store = store
+
+        drills = store.loadDrills()
+    }
+
+    func addDrill(title: String, attempts: Int, isFailable: Bool) {
+        store.createDrill(title: title, attempts: attempts, isFailable: isFailable)
+    }
 }
