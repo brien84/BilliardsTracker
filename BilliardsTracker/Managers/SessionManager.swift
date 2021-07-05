26a592b1cbb86ac32e4369a277ba3e92423c8d0a
brien84 <houseinblues@gmail.com>
2021-07-05 23:57:24 +0300

    Subscribe to `WatchCommunication`s `didReceiveResultContext` publisher

--- BilliardsTracker/Managers/SessionManager.swift
+++ BilliardsTracker/Managers/SessionManager.swift
@@ -10,9 +10,24 @@ import Foundation
 
 final class SessionManager: ObservableObject {
     private let store: DrillStore
+    private let connectivity: WatchCommunication
+
+    private(set) var selectedDrill: Drill?
+
+    private var cancellables = Set<AnyCancellable>()
 
     init(store: DrillStore, connectivity: WatchCommunication = ConnectivityManager()) {
         self.store = store
+        self.connectivity = connectivity
+
+        connectivity.didReceiveResultContext
+            .receive(on: RunLoop.main)
+            .sink { [unowned self] context in
+                if let drill = self.selectedDrill {
+                    self.addResult(context, to: drill)
+                }
+            }
+            .store(in: &cancellables)
     }
 
     private func addResult(_ context: ResultContext, to drill: Drill) {
