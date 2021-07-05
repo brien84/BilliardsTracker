49b3cd19d74a9ff56e9fb07f28f5283a0f643550
brien84 <houseinblues@gmail.com>
2021-07-05 19:47:21 +0300

    Create `StoreManager`

---
+++ BilliardsTracker/Managers/StoreManager.swift
@@ -0,0 +1,13 @@
+//
+//  StoreManager.swift
+//  BilliardsTracker
+//
+//  Created by Marius on 2021-07-05.
+//
+
+import Combine
+import Foundation
+
+final class StoreManager: ObservableObject {
+
+}
