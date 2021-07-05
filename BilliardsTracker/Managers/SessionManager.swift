cdeb8d78362e6d821a5b0e58770cffeb3d312b84
brien84 <houseinblues@gmail.com>
2021-07-05 23:59:50 +0300

    Add `start(drill:)` method

--- BilliardsTracker/Managers/SessionManager.swift
+++ BilliardsTracker/Managers/SessionManager.swift
@@ -8,11 +8,24 @@
 import Combine
 import Foundation
 
+enum SessionState: Identifiable {
+    var id: SessionState { self }
+
+    case running
+    case stopped
+    case loading
+}
+
 final class SessionManager: ObservableObject {
     private let store: DrillStore
+
     private let connectivity: WatchCommunication
+    @Published var connectivityError: ConnectivityError?
 
     private(set) var selectedDrill: Drill?
+    private(set) var startDate = Date()
+
+    @Published var runState: SessionState = .stopped
 
     private var cancellables = Set<AnyCancellable>()
 
@@ -28,6 +41,40 @@ final class SessionManager: ObservableObject {
                 }
             }
             .store(in: &cancellables)
+    }
+
+    func start(drill: Drill) {
+        guard runState == .stopped else { return }
+
+        runState = .loading
+
+        startDate = Date()
+        selectedDrill = drill
+
+        let context = DrillContext(title: drill.title, attempts: drill.attempts, isFailable: drill.isFailable, isActive: true)
+
+        connectivity.sendDrillContext(context)
+            .receive(on: RunLoop.main)
+            .subscribe(
+                Subscribers.Sink(
+                    receiveCompletion: { [weak self] completion in
+                        if completion == .finished {
+                            self?.runState = .running
+                        }
+
+                        if completion == .failure(.notReachable) {
+                            self?.runState = .stopped
+                            self?.connectivityError = .notReachable
+                        }
+
+                        if completion == .failure(.notReady) {
+                            self?.runState = .stopped
+                            self?.connectivityError = .notReady
+                        }
+                    },
+                    receiveValue: { }
+                )
+        )
     }
 
     private func addResult(_ context: ResultContext, to drill: Drill) {
