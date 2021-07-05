f3fccab5cc2f4e82581777182c669e1cb2a91c73
brien84 <houseinblues@gmail.com>
2021-07-06 00:02:09 +0300

    Add `runState` test cases

--- BilliardsTrackerTests/SessionManagerTests.swift
+++ BilliardsTrackerTests/SessionManagerTests.swift
@@ -11,14 +11,41 @@ import XCTest
 
 final class SessionManagerTests: XCTestCase {
     var sut: SessionManager!
+    var drillStore: DrillStore!
 
     override func setUpWithError() throws {
-        sut = SessionManager()
+        drillStore = try! DrillStore(inMemory: true, isPreview: true)
+        sut = SessionManager(store: drillStore)
     }
 
     override func tearDownWithError() throws {
+        drillStore = nil
         sut = nil
     }
 
+    func testStartingNewSessionSetsRunStateToLoading() throws {
+        let drill = drillStore.loadDrills().first!
+
+        sut.start(drill: drill)
+
+        XCTAssertEqual(sut.runState, .loading)
+    }
+
+    func testStartingNewSessionUpdatesStartDate() throws {
+        let drill = drillStore.loadDrills().first!
+        let initialStartDate = sut.startDate
+
+        sut.start(drill: drill)
+
+        XCTAssertNotEqual(sut.startDate, initialStartDate)
+    }
+
+    func testStartingNewSessionUpdatesSelectedDrill() throws {
+        let drill = drillStore.loadDrills().first!
+
+        sut.start(drill: drill)
+
+        XCTAssertEqual(sut.selectedDrill, drill)
+    }
 
 }
