c49177260cc4de39aeaee368a922093171b5cf39
brien84 <houseinblues@gmail.com>
2021-07-06 00:40:49 +0300

    Subscribe to `DrillStore`s `didSaveContext` publisher

--- BilliardsTracker/Managers/StoreManager.swift
+++ BilliardsTracker/Managers/StoreManager.swift
@@ -10,11 +10,27 @@ import Foundation
 
 final class StoreManager: ObservableObject {
     private let store: DrillStore
+    @Published var savingError: DrillStoreError?
 
     @Published var drills = [Drill]()
 
+    private var cancellables = Set<AnyCancellable>()
+
     init(store: DrillStore) {
         self.store = store
+
+        store.didSaveContext
+            .sink { [unowned self] result in
+                switch result {
+                case .success():
+                    drills = store.loadDrills()
+                case .failure(let error):
+                    if error == .saving {
+                        savingError = error
+                    }
+                }
+            }
+            .store(in: &cancellables)
 
         drills = store.loadDrills()
     }
