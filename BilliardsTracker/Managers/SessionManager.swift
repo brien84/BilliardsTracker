905b8d79df685e5cbb503ad2abede503d73c3bad
brien84 <houseinblues@gmail.com>
2021-07-05 19:40:42 +0300

    Create `SessionManager`

---
+++ BilliardsTracker/Managers/SessionManager.swift
@@ -0,0 +1,13 @@
+//
+//  SessionManager.swift
+//  BilliardsTracker
+//
+//  Created by Marius on 2021-07-05.
+//
+
+import Combine
+import Foundation
+
+final class SessionManager: ObservableObject {
+
+}
