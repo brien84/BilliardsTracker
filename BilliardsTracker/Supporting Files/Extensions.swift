9fcc89e597ab29b4165741106d4608fdef71e10c
brien84 <houseinblues@gmail.com>
2022-11-14 20:02:43 +0200

    Add `hidden(_:)` `View` extension method

--- BilliardsTracker/Supporting Files/Extensions.swift
+++ BilliardsTracker/Supporting Files/Extensions.swift
@@ -93,3 +93,15 @@ extension ScrollView {
         }
     }
 }
+
+/// Dinamically hides and disables view.
+///
+/// Since iOS15 the behaviour of `XCUIElement` `isHittable` property
+/// has changed in a way that it no longer returns true if `View` opacity is zero.
+/// As a workaround this function hides and also disables the `View`
+/// therefore we can now assert on `XCUIElement` `isEnabled` property.
+extension View {
+    func hidden(_ hidden: Bool) -> some View {
+        opacity(hidden ? 0 : 1).disabled(hidden)
+    }
+}
