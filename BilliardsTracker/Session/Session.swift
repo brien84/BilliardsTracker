38ccdd555497280783b7384e5413e90d31dab4ad
brien84 <houseinblues@gmail.com>
2023-01-21 16:37:04 +0200

    Create `Session` feature

---
+++ BilliardsTracker/Session/Session.swift
@@ -0,0 +1,27 @@
+//
+//  Session.swift
+//  BilliardsTracker
+//
+//  Created by Marius on 2023-01-21.
+//
+
+import ComposableArchitecture
+
+struct Session: ReducerProtocol {
+    struct State: Equatable {
+        var statistics: StatisticsManager
+    }
+
+    enum Action: Equatable {
+        case none
+    }
+
+    var body: some ReducerProtocol<State, Action> {
+        Reduce { _, action in
+            switch action {
+            case .none:
+                return .none
+            }
+        }
+    }
+}
