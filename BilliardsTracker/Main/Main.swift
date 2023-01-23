4183288ebb15345a66544274943f149d2b78b001
brien84 <houseinblues@gmail.com>
2023-01-23 20:43:03 +0200

    Create `Main` feature

---
+++ BilliardsTracker/Main/Main.swift
@@ -0,0 +1,27 @@
+//
+//  Main.swift
+//  BilliardsTracker
+//
+//  Created by Marius on 2023-01-23.
+//
+
+import ComposableArchitecture
+
+struct Main: ReducerProtocol {
+    struct State: Equatable {
+
+    }
+
+    enum Action: Equatable {
+        case none
+    }
+
+    var body: some ReducerProtocol<State, Action> {
+        Reduce { _, action in
+            switch action {
+            case .none:
+                return .none
+            }
+        }
+    }
+}
