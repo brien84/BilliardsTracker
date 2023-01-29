785a7fc0f886872339d7ecb3a571d80dfea3455e
brien84 <houseinblues@gmail.com>
2023-01-29 20:40:52 +0200

    Implement `begin()` function

--- BilliardsTracker/ConnectivityClient.swift
+++ BilliardsTracker/ConnectivityClient.swift
@@ -8,16 +8,28 @@
 import ComposableArchitecture
 import WatchConnectivity
 
+enum ConnectivityResponse: Equatable {
+    case success
+    case failure(Failure)
+
+    enum Failure: Error {
+        case notReachable
+        case notReady
+    }
+}
+
 struct ConnectivityClient {
-    var begin: @Sendable () async -> Void
+    var begin: @Sendable () async -> AsyncStream<ResultContext>
     var sendDrillContext: @Sendable (DrillContext) async -> Void
 }
 
 extension ConnectivityClient: DependencyKey {
     static var liveValue: Self {
+        let connectivity = Connectivity()
+
         return Self(
             begin: {
-
+                await connectivity.begin()
             },
             sendDrillContext: { context in
 
@@ -39,5 +51,54 @@ extension DependencyValues {
     var connectivityClient: ConnectivityClient {
         get { self[ConnectivityClient.self] }
         set { self[ConnectivityClient.self] = newValue }
+    }
+}
+
+private actor Connectivity {
+    var delegate: Delegate?
+    var session: WCSession = WCSession.default
+
+    func begin() async -> AsyncStream<ResultContext> {
+        let stream = AsyncStream { continuation in
+            self.delegate = Delegate { context in
+                continuation.yield(context)
+            }
+
+            session.delegate = delegate
+            session.activate()
+        }
+
+        return stream
+    }
+}
+
+private final class Delegate: NSObject, WCSessionDelegate {
+
+    let didReceiveResultContext: @Sendable (ResultContext) -> Void
+
+    init(didReceiveResultContext: @escaping @Sendable (ResultContext) -> Void) {
+        self.didReceiveResultContext = didReceiveResultContext
+    }
+
+    func session(_ session: WCSession, didReceiveMessageData messageData: Data) {
+        guard let context = try? JSONDecoder().decode(ResultContext.self, from: messageData) else { return }
+
+        didReceiveResultContext(context)
+    }
+
+    func session(
+        _ session: WCSession,
+        activationDidCompleteWith activationState: WCSessionActivationState,
+        error: Error?
+    ) {
+
+    }
+
+    func sessionReachabilityDidChange(_ session: WCSession) { }
+
+    func sessionDidBecomeInactive(_ session: WCSession) { }
+
+    func sessionDidDeactivate(_ session: WCSession) {
+        session.activate()
     }
 }
