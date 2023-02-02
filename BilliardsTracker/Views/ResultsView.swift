f661d172587c3173299dac3f23a86aa4123027a3
brien84 <houseinblues@gmail.com>
2023-02-02 23:58:26 +0200

    Merge `ResultView` into `ResultsView`

--- BilliardsTracker/Views/ResultsView.swift
+++ BilliardsTracker/Views/ResultsView.swift
@@ -8,40 +8,42 @@
 import SwiftUI
 
 struct ResultsView: View {
-    private let results: [DrillResult]
-
-    init(results: [DrillResult]) {
-        self.results = results
-    }
+    let results: [DrillResult]
 
     var body: some View {
         ScrollView {
             ForEach(results) { result in
-                ResultView(result: result)
+                ZStack {
+                    Color.secondaryBackground
+
+                    VStack {
+                        Text("\(result.date.asString)")
+                            .font(.caption)
+                            .foregroundColor(.secondaryElement)
+                            .padding([.horizontal, .top])
+
+                        HStack {
+                            Text("\(result.potCount)")
+                                .frame(maxWidth: .infinity)
+                                .font(.title)
+                                .foregroundColor(.customGreen)
+
+                            Text("\(result.pottingPercentage)%")
+                                .frame(maxWidth: .infinity)
+                                .font(.title3)
+                                .foregroundColor(.secondaryElement)
+
+                            Text("\(result.missCount)")
+                                .frame(maxWidth: .infinity)
+                                .font(Font.title)
+                                .foregroundColor(.customRed)
+                        }
+                        .padding()
+                    }
+                }
+
                 Divider()
             }
         }
     }
 }
-
-// swiftlint:disable force_try
-struct ResultsView_Previews: PreviewProvider {
-    static var store = try! DrillStore(inMemory: true, isPreview: true)
-    static var results = store.loadDrills().first!.results
-
-    static var view: some View {
-        ZStack {
-            Color.primaryBackground
-                .ignoresSafeArea()
-
-            ResultsView(results: results)
-                .padding()
-        }
-    }
-
-    static var previews: some View {
-        view.preferredColorScheme(.light)
-        view.preferredColorScheme(.dark)
-    }
-}
-// swiftlint:enable force_try
