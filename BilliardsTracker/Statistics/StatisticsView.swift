2f514a0844628f17bf8f6f37a54b4e1b1c4bbd89
brien84 <houseinblues@gmail.com>
2023-02-07 21:29:41 +0200

    Refactor navigation bar

--- BilliardsTracker/Statistics/StatisticsView.swift
+++ BilliardsTracker/Statistics/StatisticsView.swift
@@ -15,7 +15,7 @@ struct StatisticsView: View {
 
     private let statistics: StatisticsManager
 
-    @State private var showHistory = false
+    @State private var isShowingHistory = false
     @State private var showDeleteAlert = false
 
     init(store: StoreOf<Statistics>) {
@@ -34,16 +34,16 @@ struct StatisticsView: View {
                     StatisticsPanel(statistics: statistics)
 
                     CardView {
-                        if showHistory {
+                        if isShowingHistory {
                             if statistics.results.count < 1 {
-                                noDataLabel
+                                NotEnoughDataLabel()
                             } else {
                                 ResultsView(results: statistics.results)
                                     .accessibility(identifier: "statisticsView_resultsView")
                             }
                         } else {
                             if statistics.results.count < 2 {
-                                noDataLabel
+                                NotEnoughDataLabel()
                             } else {
                                 ChartView(dataPoints: statistics.chartDataPoints, maxValue: statistics.drill.attempts)
                                     .padding()
@@ -51,99 +51,95 @@ struct StatisticsView: View {
                             }
                         }
                     }
-                    .setTitle(showHistory ? "History" : "Performance")
-                    .setInfo(showHistory ? nil : statistics.results.count > 100 ? "Only latest 100 results are shown" : nil)
+                    .setTitle(isShowingHistory ? "History" : "Performance")
+                    .setInfo(isShowingHistory ? nil : statistics.results.count > 100 ? "Only latest 100 results are shown" : nil)
                     .id(UUID())
-                    .transition(.asymmetric(insertion: .move(edge: showHistory ? .trailing : .leading),
-                                            removal: .move(edge: showHistory ? .leading : .trailing)))
+                    .transition(
+                        .asymmetric(
+                            insertion: .move(edge: isShowingHistory ? .trailing : .leading),
+                            removal: .move(edge: isShowingHistory ? .leading : .trailing)
+                        )
+                    )
                 }
             }
-            .navigationBarTitle(statistics.drill.title)
-            .navigationBarItems(trailing: HStack(alignment: .firstTextBaseline, spacing: .navigationBarItemWidth) {
+            .alert(isPresented: $showDeleteAlert) {
+                Alert(
+                    title: Text("Confirmation"),
+                    message: Text("Are you sure you want to delete this drill?"),
+                    primaryButton: .destructive(Text("Delete")) {
+                        viewStore.send(.didTapDeleteButton)
+                        presentationMode.wrappedValue.dismiss()
+                    },
+                    secondaryButton: .cancel()
+                )
+            }
+            .toolbar {
+                ToolbarItemGroup {
+                    deleteButton
+                        .frame(width: Self.toolbarItemWidth)
 
-                Button {
-                    showDeleteAlert = true
-                } label: {
-                    Image(systemName: "trash")
-                        .font(Font.body)
-                        .imageScale(.large)
-                        .foregroundColor(.customRed)
+                    toggleViewButton
+                        .foregroundColor(statistics.results.isEmpty ? .secondaryElement : .primaryElement)
+                        .disabled(statistics.results.isEmpty)
+                        .frame(width: Self.toolbarItemWidth)
                 }
-                .alert(isPresented: $showDeleteAlert, content: {
-                    Alert(
-                        title: Text("Confirmation"),
-                        message: Text("Are you sure you want to delete this drill?"),
-                        primaryButton: .destructive(Text("Delete")) {
-                            viewStore.send(.didTapDeleteButton)
-                            presentationMode.wrappedValue.dismiss()
-                        },
-                        secondaryButton: .cancel()
-                    )
-                })
-                .frame(width: .navigationBarItemWidth)
-                .accessibility(identifier: "statisticsView_deleteButton")
-
-                toggleHistoryButton.frame(width: .navigationBarItemWidth)
-            })
+            }
+            .navigationTitle(statistics.drill.title)
         }
     }
 
-    private var toggleHistoryButton: some View {
+    private var deleteButton: some View {
+        Button {
+            showDeleteAlert = true
+        } label: {
+            Image(systemName: "trash")
+                .font(.body)
+                .imageScale(.large)
+                .foregroundColor(.customRed)
+        }
+    }
+
+    private var toggleViewButton: some View {
         Button {
             withAnimation {
-                showHistory.toggle()
+                isShowingHistory.toggle()
             }
         } label: {
-            Image(systemName: showHistory ? "chart.bar.xaxis" : "tray.full")
-                .font(Font.body)
+            Image(systemName: isShowingHistory ? "chart.bar.xaxis" : "tray.full")
+                .font(.body)
                 .imageScale(.large)
         }
-        .disabled(statistics.results.isEmpty)
-        .foregroundColor(statistics.results.isEmpty ? .secondaryElement : .primaryElement)
-        .accessibility(identifier: "statisticsView_toggleHistoryButton")
     }
+}
 
-    private var noDataLabel: some View {
-        VStack(spacing: .noDataLabelSpacing) {
+private struct NotEnoughDataLabel: View {
+    var body: some View {
+        VStack(spacing: Self.verticalSpacing) {
             Image("pocket")
                 .resizable()
                 .aspectRatio(contentMode: .fit)
-                .frame(width: .noDataLabelWidth, height: .noDataLabelHeight)
+                .frame(width: Self.width, height: Self.height)
 
             Text("Not enough data")
                 .font(.title3)
                 .foregroundColor(.primaryElement)
         }
         .frame(maxWidth: .infinity, maxHeight: .infinity)
-        .offset(.noDataLabelOffset)
-        .accessibility(identifier: "statisticsView_noDataLabel")
+        .offset(Self.offset)
     }
 }
 
 // MARK: - Constants
 
-private extension CGFloat {
-    static var navigationBarItemWidth: CGFloat {
-        30
-    }
-
-    static var noDataLabelSpacing: CGFloat {
-        16
-    }
-
-    static var noDataLabelWidth: CGFloat {
-        100
-    }
-
-    static var noDataLabelHeight: CGFloat {
-        100
-    }
+private extension StatisticsView {
+    static let toolbarItemWidth: CGFloat = 32
 }
 
-private extension CGSize {
-    static var noDataLabelOffset: CGSize {
-        CGSize(width: 0, height: -32)
-    }
+private extension NotEnoughDataLabel {
+    static let verticalSpacing: CGFloat = 16
+    static let height: CGFloat = 100
+    static let width: CGFloat = 100
+    static let offset: CGSize = CGSize(width: 0, height: -32)
 }
 
 // MARK: - Previews
