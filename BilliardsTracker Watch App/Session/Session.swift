a592e7056906a8cee3a364fb36cc723281401e74
brien84 <houseinblues@gmail.com>
2023-02-17 23:16:17 +0200

    Implement `didRegisterShot(isSuccess:)` action

--- BilliardsTracker Watch App/Session/Session.swift
+++ BilliardsTracker Watch App/Session/Session.swift
@@ -25,11 +25,19 @@ struct Session: ReducerProtocol {
 
         var isPaused = false
 
+        var remainingShots: Int {
+            let shots = shotCount - potCount - missCount
+            guard shots > 0 else { return 0 }
+            return shots
+        }
+
         @BindingState var currentTab: Session.Tab = .progress
     }
 
     enum Action: BindableAction, Equatable {
         case binding(BindingAction<State>)
+
+        case didRegisterShot(isSuccess: Bool)
 
         case pauseButtonDidTap
         case resumeButtonDidTap
@@ -43,6 +51,19 @@ struct Session: ReducerProtocol {
             case .binding:
                 return .none
 
+            case .didRegisterShot(let isSuccess):
+                guard state.remainingShots > 0 else { return .none }
+
+                if isSuccess {
+                    state.potCount += 1
+                    WKInterfaceDevice().play(.notification)
+                } else {
+                    state.missCount += 1
+                    WKInterfaceDevice().play(.failure)
+                }
+
+                return .none
+
             case .pauseButtonDidTap:
                 state.isPaused = true
                 state.currentTab = .progress
@@ -54,7 +75,6 @@ struct Session: ReducerProtocol {
                 state.currentTab = .progress
                 WKInterfaceDevice().play(.directionUp)
                 return .none
-
             }
         }
     }
--- BilliardsTracker Watch App/Session/SessionProgressView.swift
+++ BilliardsTracker Watch App/Session/SessionProgressView.swift
@@ -8,11 +8,55 @@
 import ComposableArchitecture
 import SwiftUI
 
+private extension SessionProgressView {
+    struct State: Equatable {
+        let title: String
+        let shotCount: Int
+        let potCount: Int
+        let missCount: Int
+        let remainingShots: Int
+        let isPaused: Bool
+
+        var remainingShotsPercentage: CGFloat {
+            guard shotCount > 0 else { return 0 }
+            return CGFloat(remainingShots) / CGFloat(shotCount)
+        }
+
+        init(session: Session.State) {
+            self.title = session.title
+            self.shotCount = session.shotCount
+            self.potCount = session.potCount
+            self.missCount = session.missCount
+            self.remainingShots = session.remainingShots
+            self.isPaused = session.isPaused
+        }
+    }
+
+    enum Action: Equatable {
+        case didRegisterShot(isSuccess: Bool)
+    }
+}
+
+private extension Session.State {
+    var state: SessionProgressView.State {
+        .init(session: self)
+    }
+}
+
+private extension SessionProgressView.Action {
+    var action: Session.Action {
+        switch self {
+        case .didRegisterShot(isSuccess: let isSuccess):
+            return .didRegisterShot(isSuccess: isSuccess)
+        }
+    }
+}
+
 struct SessionProgressView: View {
     let store: StoreOf<Session>
 
     var body: some View {
-        WithViewStore(store) { viewStore in
+        WithViewStore(store, observe: \.state, send: \Action.action) { viewStore in
             VStack {
                 MarqueeText(viewStore.title, font: .headline)
                     .padding(.top)
@@ -20,9 +64,19 @@ struct SessionProgressView: View {
 
                 Spacer()
 
+                ProgressView(value: viewStore.remainingShotsPercentage) {
+                    Text("\(viewStore.shotCount - viewStore.potCount - viewStore.missCount)")
+                        .bold()
+                        .foregroundColor(.primaryElement)
+                }
+                .progressViewStyle(.circular)
+                .tint(viewStore.isPaused ? .secondaryElement : .customBlue)
+
+                Spacer()
+
                 HStack {
                     Button {
-
+                        viewStore.send(.didRegisterShot(isSuccess: true), animation: .default)
                     } label: {
                         Text("\(viewStore.potCount)")
                             .foregroundColor(.customGreen)
@@ -31,7 +85,7 @@ struct SessionProgressView: View {
                     .tint(.customGreen)
 
                     Button {
-
+                        viewStore.send(.didRegisterShot(isSuccess: false), animation: .default)
                     } label: {
                         Text("\(viewStore.missCount)")
                             .foregroundColor(.customRed)
@@ -39,6 +93,7 @@ struct SessionProgressView: View {
                     .buttonStyle(.bordered)
                     .tint(.customRed)
                 }
+                .disabled(viewStore.isPaused)
             }
         }
     }
