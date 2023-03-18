16021fee20d9bf46c256cd93156dc0cc03695fe1
brien84 <houseinblues@gmail.com>
2023-03-18 20:19:30 +0200

    Create Watch App unit test bundle

---
+++ BilliardsTrackerWatchAppTests/BilliardsTrackerWatchAppTests.swift
@@ -0,0 +1,16 @@
+//
+//  BilliardsTracker_Watch_AppTests.swift
+//  BilliardsTracker Watch AppTests
+//
+//  Created by Marius on 2023-03-18.
+//
+
+import XCTest
+
+final class BilliardsTrackerWatchAppTests: XCTestCase {
+
+    func testExample() throws {
+
+    }
+
+}
