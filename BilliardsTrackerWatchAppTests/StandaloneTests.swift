550d68a2e0615c42d3a23310f4d9e953aa150922
brien84 <houseinblues@gmail.com>
2023-03-24 17:38:40 +0200

    Create `SessionTests`, `StandaloneTests`, `TrackedTests` unit test case

---
+++ BilliardsTrackerWatchAppTests/StandaloneTests.swift
@@ -0,0 +1,24 @@
+//
+//  StandaloneTests.swift
+//  BilliardsTrackerWatchAppTests
+//
+//  Created by Marius on 2023-03-24.
+//
+
+import XCTest
+
+final class StandaloneTests: XCTestCase {
+
+    override func setUpWithError() throws {
+
+    }
+
+    override func tearDownWithError() throws {
+
+    }
+
+    func testExample() throws {
+
+    }
+
+}
---
+++ BilliardsTrackerWatchAppTests/TrackedTests.swift
@@ -0,0 +1,24 @@
+//
+//  TrackedTests.swift
+//  BilliardsTrackerWatchAppTests
+//
+//  Created by Marius on 2023-03-24.
+//
+
+import XCTest
+
+final class TrackedTests: XCTestCase {
+
+    override func setUpWithError() throws {
+
+    }
+
+    override func tearDownWithError() throws {
+
+    }
+
+    func testExample() throws {
+ 
+    }
+
+}
