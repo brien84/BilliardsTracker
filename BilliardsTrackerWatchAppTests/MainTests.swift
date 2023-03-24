a0c237c00d0c3bd2264d160eccbdf0be5935386c
brien84 <houseinblues@gmail.com>
2023-03-24 17:35:45 +0200

    Create `MainTests` test case

---
+++ BilliardsTrackerWatchAppTests/MainTests.swift
@@ -0,0 +1,24 @@
+//
+//  MainTests.swift
+//  BilliardsTrackerWatchAppTests
+//
+//  Created by Marius on 2023-03-24.
+//
+
+import XCTest
+
+final class MainTests: XCTestCase {
+
+    override func setUpWithError() throws {
+
+    }
+
+    override func tearDownWithError() throws {
+
+    }
+
+    func testExample() throws {
+
+    }
+
+}
