ceb6b55e47ae4f2b469e785c5cdff73789c88ccb
brien84 <houseinblues@gmail.com>
2023-04-01 21:49:09 +0300

    Create `UserDefaultsClient`

---
+++ BilliardsTracker/Clients/UserDefaultsClient/LiveUserDefaultsClient.swift
@@ -0,0 +1,33 @@
+//
+//  LiveUserDefaultsClient.swift
+//  BilliardsTracker
+//
+//  Created by Marius on 2023-04-01.
+//
+
+import Dependencies
+import Foundation
+
+extension UserDefaultsClient: DependencyKey {
+    static let liveValue = Self(
+        getSortOption: {
+            let rawValue = UserDefaults.standard.integer(forKey: Self.sortOptionKey)
+            return SortOption(rawValue: rawValue) ?? .title
+        },
+        setSortOption: { option in
+            UserDefaults.standard.set(option.rawValue, forKey: Self.sortOptionKey)
+        },
+        getSortOrder: {
+            let rawValue = UserDefaults.standard.bool(forKey: Self.sortOrderKey)
+            return rawValue ? .forward : .reverse
+        },
+        setSortOrder: { order in
+            UserDefaults.standard.set(order == .forward, forKey: Self.sortOrderKey)
+        }
+    )
+}
+
+private extension UserDefaultsClient {
+    static let sortOptionKey = "sortOptionKey"
+    static let sortOrderKey = "sortOrderKey"
+}
---
+++ BilliardsTracker/Clients/UserDefaultsClient/UserDefaultsClient.swift
@@ -0,0 +1,62 @@
+//
+//  UserDefaultsClient.swift
+//  BilliardsTracker
+//
+//  Created by Marius on 2023-04-01.
+//
+
+import Dependencies
+import Foundation
+
+struct UserDefaultsClient {
+    var getSortOption: @Sendable () -> SortOption
+    var setSortOption: @Sendable (SortOption) async -> Void
+    var getSortOrder: @Sendable () -> SortOrder
+    var setSortOrder: @Sendable (SortOrder) async -> Void
+}
+
+extension UserDefaultsClient: TestDependencyKey {
+    static let previewValue: Self = {
+        let userDefaults = { UserDefaults(suiteName: "UserDefaultsClient.preview")! }
+        userDefaults().removePersistentDomain(forName: "UserDefaultsClient.preview")
+
+        return Self(
+            getSortOption: {
+                let rawValue = userDefaults().integer(forKey: "sortOptionKey")
+                return SortOption(rawValue: rawValue) ?? .title
+            },
+            setSortOption: { option in
+                userDefaults().set(option.rawValue, forKey: "sortOptionKey")
+            },
+            getSortOrder: {
+                let rawValue = userDefaults().bool(forKey: "sortOrderKey")
+                return rawValue ? .forward : .reverse
+            },
+            setSortOrder: { order in
+                userDefaults().set(order == .forward, forKey: "sortOrderKey")
+            }
+        )
+    }()
+
+    static let testValue = Self(
+        getSortOption: {
+            unimplemented("\(Self.self).getSortOption")
+        },
+        setSortOption: { _ in
+            unimplemented("\(Self.self).setSortOption")
+        },
+        getSortOrder: {
+            unimplemented("\(Self.self).getSortOrder")
+        },
+        setSortOrder: { _ in
+            unimplemented("\(Self.self).setSortOrder")
+        }
+    )
+}
+
+extension DependencyValues {
+    var userDefaults: UserDefaultsClient {
+        get { self[UserDefaultsClient.self] }
+        set { self[UserDefaultsClient.self] = newValue }
+    }
+}
