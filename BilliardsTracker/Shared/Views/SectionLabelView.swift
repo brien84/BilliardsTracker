7428861dcf128561f6a852c9d5b265ca9df23477
brien84 <houseinblues@gmail.com>
2023-04-16 23:59:04 +0300

    Create `SectionLabelView`

---
+++ BilliardsTracker/Shared/Views/SectionLabelView.swift
@@ -0,0 +1,35 @@
+//
+//  SectionLabelView.swift
+//  BilliardsTracker
+//
+//  Created by Marius on 2023-04-16.
+//
+
+import SwiftUI
+
+struct SectionLabelView: View {
+    let title: String
+
+    var body: some View {
+        Text(title)
+            .font(.title2.bold())
+            .foregroundColor(.primaryElement)
+            .frame(maxWidth: .infinity, alignment: .leading)
+            .padding([.horizontal, .top])
+            .padding(.bottom, Self.bottomPadding)
+    }
+}
+
+// MARK: - Constants
+
+private extension SectionLabelView {
+    static let bottomPadding: CGFloat = 8
+}
+
+// MARK: - Previews
+
+struct SectionLabelView_Previews: PreviewProvider {
+    static var previews: some View {
+        SectionLabelView(title: "Preview")
+    }
+}
