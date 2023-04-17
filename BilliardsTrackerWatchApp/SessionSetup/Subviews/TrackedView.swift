f59e3aff43003433a5ae4c646640b805cdf04679
brien84 <houseinblues@gmail.com>
2023-04-17 16:13:34 +0300

    Add image illustration to `TrackedView` for improved visuals

--- BilliardsTrackerWatchApp/SessionSetup/Subviews/TrackedView.swift
+++ BilliardsTrackerWatchApp/SessionSetup/Subviews/TrackedView.swift
@@ -41,19 +41,43 @@ struct TrackedView: View {
 
     var body: some View {
         WithViewStore(store, observe: \.state, send: \Action.action) { viewStore in
-            Text("Select a drill on BilliardsTracker iPhone application")
-                .multilineTextAlignment(.center)
-                .font(.caption)
-                .foregroundColor(.primaryElement)
-                .navigationBarBackButtonHidden(viewStore.isNavigationToSessionActive)
-                .onAppear {
-                    viewStore.send(.establishConnection)
+            VStack(spacing: Self.verticalSpacing) {
+                ZStack(alignment: .bottom) {
+                    Image(systemName: "iphone")
+                        .font(.system(size: Self.iphoneFontSize, weight: .ultraLight))
+                        .foregroundColor(.accentColor)
+                        .offset(Self.iphoneOffset)
+
+                    Image("icon")
+                        .resizable()
+                        .frame(width: Self.iconSize.width, height: Self.iconSize.height)
+                        .offset(Self.iconOffset)
                 }
-                .onDisappear {
-                    viewStore.send(.endConnection)
-                }
+
+                Text("Select a drill on BilliardsTracker iPhone application")
+                    .multilineTextAlignment(.center)
+                    .font(.caption)
+                    .foregroundColor(.primaryElement)
+            }
+            .navigationBarBackButtonHidden(viewStore.isNavigationToSessionActive)
+            .onAppear {
+                viewStore.send(.establishConnection)
+            }
+            .onDisappear {
+                viewStore.send(.endConnection)
+            }
         }
     }
+}
+
+// MARK: - Constants
+
+private extension TrackedView {
+    static let iconSize: CGSize = CGSize(width: 36, height: 36)
+    static let iphoneFontSize: CGFloat = 72
+    static let iconOffset: CGSize = CGSize(width: 12, height: -5)
+    static let iphoneOffset: CGSize = CGSize(width: -12, height: 0)
+    static let verticalSpacing: CGFloat = 16
 }
 
 // MARK: - Previews
