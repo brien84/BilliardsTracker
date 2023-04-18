0736e9cf2dd51a30149c9394d0faf36f7c645a73
brien84 <houseinblues@gmail.com>
2023-04-18 22:27:35 +0300

    Create `IntegrationTests` suite

---
+++ BilliardsTrackerTests/IntegrationTests.swift
@@ -0,0 +1,21 @@
+//
+//  IntegrationTests.swift
+//  BilliardsTrackerTests
+//
+//  Created by Marius on 2023-04-18.
+//
+
+import ComposableArchitecture
+import XCTest
+@testable import BilliardsTracker
+
+@MainActor
+final class IntegrationTests: XCTestCase {
+    override func setUp() async throws {
+
+    }
+
+    override func tearDown() async throws {
+
+    }
+}
