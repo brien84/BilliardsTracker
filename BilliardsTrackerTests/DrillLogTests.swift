d57adf8ecf4e1ce4cc21de4464876629723fd0b3
brien84 <houseinblues@gmail.com>
2023-04-19 19:52:19 +0300

    Test `DrillLog` feature

--- BilliardsTrackerTests/DrillLogTests.swift
+++ BilliardsTrackerTests/DrillLogTests.swift
@@ -11,5 +11,29 @@ import XCTest
 
 @MainActor
 final class DrillLogTests: XCTestCase {
+    func testOpeningAndClosingFullHistoryView() async throws {
+        let drill = PersistenceClient.mockDrill
+        let store = TestStore(initialState: DrillLog.State(drill: drill), reducer: DrillLog())
 
+        await store.send(.didPressShowFullHistoryButton) {
+            $0.isNavigationToFullHistoryActive = true
+        }
+
+        await store.send(.didPressExitFullHistoryButton) {
+            $0.isNavigationToFullHistoryActive = false
+        }
+    }
+
+    func testAlertConfirmationBeforeDeletingDrill() async throws {
+        let drill = PersistenceClient.mockDrill
+        let store = TestStore(initialState: DrillLog.State(drill: drill), reducer: DrillLog())
+
+        await store.send(.didPressDeleteButton) {
+            $0.alert = DrillLog().deletionAlert
+        }
+
+        await store.send(.alertDidDismiss) {
+            $0.alert = nil
+        }
+    }
 }
