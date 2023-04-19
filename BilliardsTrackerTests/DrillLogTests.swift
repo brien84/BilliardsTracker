0a29af92d40e4826a0cf8718042f637dfba3c69c
brien84 <houseinblues@gmail.com>
2023-04-19 19:49:28 +0300

    Create `DrillLogTests` suite

---
+++ BilliardsTrackerTests/DrillLogTests.swift
@@ -0,0 +1,15 @@
+//
+//  DrillLogTests.swift
+//  BilliardsTrackerTests
+//
+//  Created by Marius on 2023-04-19.
+//
+
+import ComposableArchitecture
+import XCTest
+@testable import BilliardsTracker
+
+@MainActor
+final class DrillLogTests: XCTestCase {
+
+}
