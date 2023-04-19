769d3977073b5b6b8c224412796c2fe08f42fb97
brien84 <houseinblues@gmail.com>
2023-04-19 19:48:35 +0300

    Create `SettingsTests` suite

---
+++ BilliardsTrackerTests/SettingsTests.swift
@@ -0,0 +1,15 @@
+//
+//  SettingsTests.swift
+//  BilliardsTrackerTests
+//
+//  Created by Marius on 2023-04-19.
+//
+
+import ComposableArchitecture
+import XCTest
+@testable import BilliardsTracker
+
+@MainActor
+final class SettingsTests: XCTestCase {
+
+}
