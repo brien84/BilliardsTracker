351cdd0da7479a189b4447ee8635c92530c50b4c
brien84 <houseinblues@gmail.com>
2023-04-19 19:50:34 +0300

    Create `SessionTests` suite

---
+++ BilliardsTrackerTests/SessionTests.swift
@@ -0,0 +1,15 @@
+//
+//  SessionTests.swift
+//  BilliardsTrackerTests
+//
+//  Created by Marius on 2023-04-19.
+//
+
+import ComposableArchitecture
+import XCTest
+@testable import BilliardsTracker
+
+@MainActor
+final class SessionTests: XCTestCase {
+
+}
