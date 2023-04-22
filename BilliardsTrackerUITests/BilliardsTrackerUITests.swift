db58c979405f8492a64edf24c6e4395d447fea90
brien84 <houseinblues@gmail.com>
2023-04-22 22:53:22 +0300

    Update UI tests

--- BilliardsTrackerUITests/BilliardsTrackerUITests.swift
+++ BilliardsTrackerUITests/BilliardsTrackerUITests.swift
@@ -7,352 +7,101 @@
 
 import XCTest
 
-// swiftlint:disable force_try
-// swiftlint:disable identifier_name
 final class BilliardsTrackerUITests: XCTestCase {
     var app: XCUIApplication!
 
     override func setUpWithError() throws {
         continueAfterFailure = false
+        app = XCUIApplication()
+        app.launchArguments = ["ui-testing"]
+        app.launch()
     }
 
     override func tearDownWithError() throws {
         app = nil
     }
 
-    private func startApp(data: Bool) {
-        app = XCUIApplication()
-        app.launchArguments = data ? ["ui-testing"] : ["ui-testing-no-data"]
-        app.launch()
+    func testUI() throws {
+        closeOnboardView()
+        openNewDrillViewByNavigationBarButton()
+        cancelNewDrillView()
+        openNewDrillViewByEmptyDrillPrompt()
+        saveNewDrillView()
+        openSessionView()
+        closeSessionView()
+        openDrillLogView()
+        deleteDrill()
     }
 
-    // MARK: - CreateDrillView
-
-    func testOpeningCreateDrillViewWithNavigationButton() throws {
-        startApp(data: false)
-
-        XCTAssertTrue(app.mainView.waitForExistence(timeout: 1.0))
-
-        app.mainView_createDrillButtonNavigation.tap()
-
-        XCTAssertTrue(app.createDrillView.waitForExistence(timeout: 1.0))
+    func closeOnboardView() {
+        let element = app.buttons["Continue"]
+        XCTAssertTrue(element.waitForExistence(timeout: 1.0))
+        element.tap()
     }
 
-    func testOpeningCreateDrillViewWithBackgroundButton() throws {
-        startApp(data: false)
-
-        XCTAssertTrue(app.mainView.waitForExistence(timeout: 1.0))
-
-        app.mainView_createDrillButtonBackground.tap()
-
-        XCTAssertTrue(app.createDrillView.waitForExistence(timeout: 1.0))
+    func openNewDrillViewByNavigationBarButton() {
+        let element = app.navigationBars.buttons["Add"]
+        XCTAssertTrue(element.waitForExistence(timeout: 1.0))
+        element.tap()
     }
 
-    func testClosingCreateDrillView() throws {
-        try! testOpeningCreateDrillViewWithNavigationButton()
-
-        app.buttons["Cancel"].tap()
-
-        XCTAssertFalse(app.createDrillView.isHittable)
+    func openNewDrillViewByEmptyDrillPrompt() {
+        let element = app.buttons["Add Drill"]
+        XCTAssertTrue(element.waitForExistence(timeout: 1.0))
+        element.tap()
     }
 
-    func testTogglingCreateDrillFailableHelpView() throws {
-        try! testOpeningCreateDrillViewWithNavigationButton()
-
-        app.createDrillView_infoButton.tap()
-        XCTAssertTrue(app.createDrillView_failableHelpView.isHittable)
-
-        app.createDrillView_infoButton.tap()
-        XCTAssertFalse(app.createDrillView_failableHelpView.isHittable)
-
-        app.createDrillView_infoButton.tap()
-        XCTAssertTrue(app.createDrillView_failableHelpView.isHittable)
-
-        app.createDrillView_failableHelpView.tap()
-        XCTAssertFalse(app.createDrillView_failableHelpView.isHittable)
+    func cancelNewDrillView() {
+        let element = app.buttons["Cancel"]
+        XCTAssertTrue(element.waitForExistence(timeout: 1.0))
+        element.tap()
     }
 
-    func testCreatingDrill() throws {
-        let title = "testTitle"
-        let attempts = 30
-        let isFailable = true
+    func saveNewDrillView() {
+        let textField = app.textFields["Drill Title"]
+        textField.tap()
+        textField.typeText("Test Drill")
+        app.keyboards.firstMatch.buttons["return"].tap()
 
-        try! testOpeningCreateDrillViewWithNavigationButton()
+        app.staticTexts["Shots"].tap()
+        app.pickerWheels.firstMatch.adjust(toPickerWheelValue: "10")
 
-        // types title into TitleTextField
-        app.textFields.firstMatch.tap()
-        app.textFields.firstMatch.typeText(title)
-
-        // set attempts
-        // slider adjusment behaviour changed on iOS15 and onwards
-        if #available(iOS 16.0, *) {
-            app.sliders.firstMatch.adjust(toNormalizedSliderPosition: CGFloat(attempts - 2) / 100.0)
-        } else {
-            app.sliders.firstMatch.adjust(toNormalizedSliderPosition: CGFloat(attempts - 1) / 100.0)
-        }
-
-        XCTAssertEqual(String(attempts), app.createDrillView_attemptsText.label)
-
-        // toggle isFailable Toggle
-        app.switches.firstMatch.tap()
+        app.switches["Continuous"].tap()
 
         app.buttons["Save"].tap()
-
-        XCTAssertTrue(app.drillView_titleText.waitForExistence(timeout: 1.0))
-        XCTAssertEqual(title.uppercased(), app.drillView_titleText.label)
-        XCTAssertEqual(String(attempts), app.drillView_attemptsText.label)
-        XCTAssertEqual(isFailable, app.drillView_failableIcon.isHittable)
     }
 
-    func testCreatingDrillLargeInputs() throws {
-        let title = String(repeating: "a", count: 500)
-        let attempts = 100
-        let isFailable = true
-
-        try! testOpeningCreateDrillViewWithNavigationButton()
-
-        // types title into TitleTextField
-        app.textFields.firstMatch.tap()
-        app.textFields.firstMatch.typeText(title)
-
-        // set attempts
-        app.sliders.firstMatch.adjust(toNormalizedSliderPosition: CGFloat(attempts) / 100.0)
-        XCTAssertEqual(String(attempts), app.createDrillView_attemptsText.label)
-
-        // toggle isFailable Toggle
-        app.switches.firstMatch.tap()
-
-        app.buttons["Save"].tap()
-
-        XCTAssertTrue(app.drillView_titleText.waitForExistence(timeout: 1.0))
-        XCTAssertEqual(title.uppercased(), app.drillView_titleText.label)
-        XCTAssertEqual(String(attempts), app.drillView_attemptsText.label)
-        XCTAssertEqual(isFailable, app.drillView_failableIcon.isEnabled)
+    func openSessionView() {
+        let element = app.staticTexts["TEST DRILL"]
+        XCTAssertTrue(element.waitForExistence(timeout: 1.0))
+        element.tap()
     }
 
-    func testCreatingDrillEmptyInputs() throws {
-        let title = ""
-        let attempts = 0
+    func closeSessionView() {
+        let element = app.buttons["Close"]
+        XCTAssertTrue(element.waitForExistence(timeout: 1.0))
+        element.tap()
 
-        try! testOpeningCreateDrillViewWithNavigationButton()
-
-        // types title into TitleTextField
-        app.textFields.firstMatch.tap()
-        app.textFields.firstMatch.typeText(title)
-
-        // set attempts
-        app.sliders.firstMatch.adjust(toNormalizedSliderPosition: CGFloat(attempts) / 100.0)
-        XCTAssertEqual(String(1), app.createDrillView_attemptsText.label)
-
-        app.buttons["Save"].tap()
-
-        XCTAssertTrue(app.drillView_titleText.waitForExistence(timeout: 1.0))
-        XCTAssertEqual("DRILL TITLE", app.drillView_titleText.label)
-        XCTAssertEqual("1", app.drillView_attemptsText.label)
-        XCTAssertFalse(app.drillView_failableIcon.isEnabled)
+        let alert = app.alerts["Confirmation"]
+        XCTAssertTrue(element.waitForExistence(timeout: 1.0))
+        alert.buttons["Yes"].tap()
+        XCTAssertFalse(element.waitForExistence(timeout: 1.0))
     }
 
-    // MARK: - SettingsView
-
-    func testOpeningSettingsView() throws {
-        startApp(data: true)
-
-        app.mainView_settingsButton.tap()
-
-        XCTAssertTrue(app.settingsView_titleText.waitForExistence(timeout: 1.0))
-        XCTAssertTrue(app.settingsView_titleText.isHittable)
+    func openDrillLogView() {
+        let element = app.buttons["Open Drill Log"]
+        XCTAssertTrue(element.waitForExistence(timeout: 1.0))
+        element.tap()
     }
 
-    func testClosingSettingsView() throws {
-        try! testOpeningSettingsView()
+    func deleteDrill() {
+        let element = app.buttons["Trash"]
+        XCTAssertTrue(element.waitForExistence(timeout: 1.0))
+        element.tap()
 
-        app.mainView_settingsButton.tap()
-
-        XCTAssertFalse(app.settingsView_titleText.isHittable)
+        let alert = app.alerts["Confirmation"]
+        XCTAssertTrue(element.waitForExistence(timeout: 1.0))
+        alert.buttons["Delete"].tap()
+        XCTAssertFalse(element.waitForExistence(timeout: 1.0))
     }
-
-    func testClosingSettingsViewByClickingOutOfBounds() throws {
-        try! testOpeningSettingsView()
-
-        app.drillView_statisticsButton.firstMatch.tap()
-
-        XCTAssertFalse(app.settingsView_titleText.isHittable)
-    }
-
-    func testSelectingSortOption() throws {
-        try! testOpeningSettingsView()
-
-        if !app.settingsView_attemptsImage.isEnabled {
-            app.settingsView_attemptsText.tap()
-            XCTAssertTrue(app.settingsView_attemptsImage.isEnabled)
-        } else if !app.settingsView_titleImage.isEnabled {
-            app.settingsView_titleText.tap()
-            XCTAssertTrue(app.settingsView_titleImage.isEnabled)
-        } else {
-            XCTFail("Two or more options are selected at the same time.")
-        }
-    }
-
-    // MARK: - StatisticsView
-
-    func testOpeningStatisticsView() throws {
-        startApp(data: true)
-
-        app.drillView_statisticsButton.firstMatch.tap()
-
-        XCTAssertTrue(app.statisticsView_toggleHistoryButton.exists)
-    }
-
-    func testClosingStatisticsView() throws {
-        try! testOpeningStatisticsView()
-
-        let backButton = app.navigationBars.firstMatch.buttons.firstMatch
-        backButton.tap()
-
-        XCTAssertFalse(app.statisticsView_toggleHistoryButton.exists)
-    }
-
-    func testTogglingBetweenChartAndResultsViews() throws {
-        try! testOpeningStatisticsView()
-
-        XCTAssertTrue(app.statisticsView_chartView.exists)
-        XCTAssertFalse(app.statisticsView_resultsView.exists)
-
-        app.statisticsView_toggleHistoryButton.tap()
-
-        XCTAssertTrue(app.statisticsView_resultsView.exists)
-        XCTAssertFalse(app.statisticsView_chartView.exists)
-    }
-
-    func testDrillWithNoResultsDisplaysNoDataLabel() throws {
-        try! testCreatingDrill()
-
-        XCTAssertFalse(app.statisticsView_noDataLabel.exists)
-
-        app.drillView_statisticsButton.tap()
-
-        XCTAssertTrue(app.statisticsView_noDataLabel.exists)
-    }
-
-    func testDrillWithNoResultsToggleButtonIsDisabled() throws {
-        try! testCreatingDrill()
-
-        app.drillView_statisticsButton.tap()
-
-        XCTAssertTrue(app.statisticsView_toggleHistoryButton.exists)
-        XCTAssertFalse(app.statisticsView_toggleHistoryButton.isEnabled)
-    }
-
-    func testDeletingDrill() throws {
-        try! testCreatingDrill()
-
-        app.drillView_statisticsButton.tap()
-
-        XCTAssertTrue(app.statisticsView_deleteButton.exists)
-        app.statisticsView_deleteButton.tap()
-
-        let confirmationButton = app.alerts.firstMatch.buttons.element(boundBy: 1)
-        confirmationButton.tap()
-
-        XCTAssertFalse(app.statisticsView_deleteButton.isHittable)
-        XCTAssertFalse(app.drillView_statisticsButton.exists)
-    }
-}
-
-extension XCUIApplication {
-
-    // MARK: - CreateDrillView
-
-    var createDrillView: XCUIElement {
-        otherElements["createDrillView"]
-    }
-
-    var createDrillView_attemptsText: XCUIElement {
-        staticTexts["createDrillView_attemptsText"]
-    }
-
-    var createDrillView_infoButton: XCUIElement {
-        buttons["createDrillView_infoButton"]
-    }
-
-    var createDrillView_failableHelpView: XCUIElement {
-        staticTexts["createDrillView_failableHelpView"]
-    }
-
-    // MARK: - DrillView
-
-    var drillView_attemptsText: XCUIElement {
-        staticTexts["drillView_attemptsText"]
-    }
-
-    var drillView_titleText: XCUIElement {
-        staticTexts["drillView_titleText"]
-    }
-
-    var drillView_failableIcon: XCUIElement {
-        images["drillView_failableIcon"]
-    }
-
-    var drillView_statisticsButton: XCUIElement {
-        buttons["drillView_statisticsButton"]
-    }
-
-    // MARK: - MainView
-
-    var mainView: XCUIElement {
-        otherElements["mainView"]
-    }
-
-    var mainView_createDrillButtonBackground: XCUIElement {
-        buttons["mainView_createDrillButtonBackground"]
-    }
-
-    var mainView_createDrillButtonNavigation: XCUIElement {
-        buttons["mainView_createDrillButtonNavigation"]
-    }
-
-    var mainView_settingsButton: XCUIElement {
-        buttons["mainView_settingsButton"]
-    }
-
-    // MARK: - SettingsView
-
-    var settingsView_attemptsText: XCUIElement {
-        staticTexts["settingsView_attemptsText"]
-    }
-
-    var settingsView_titleText: XCUIElement {
-        staticTexts["settingsView_titleText"]
-    }
-
-    var settingsView_attemptsImage: XCUIElement {
-        images["settingsView_attemptsImage"]
-    }
-
-    var settingsView_titleImage: XCUIElement {
-        images["settingsView_titleImage"]
-    }
-
-    // MARK: - StatisticsView
-
-    var statisticsView_deleteButton: XCUIElement {
-        buttons["statisticsView_deleteButton"]
-    }
-
-    var statisticsView_toggleHistoryButton: XCUIElement {
-        buttons["statisticsView_toggleHistoryButton"]
-    }
-
-    var statisticsView_chartView: XCUIElement {
-        staticTexts["statisticsView_chartView"]
-    }
-
-    var statisticsView_resultsView: XCUIElement {
-        scrollViews["statisticsView_resultsView"]
-    }
-
-    var statisticsView_noDataLabel: XCUIElement {
-        staticTexts["statisticsView_noDataLabel"]
-    }
-
 }
