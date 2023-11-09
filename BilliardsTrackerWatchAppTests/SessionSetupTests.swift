47c8dc081a39376de8c111b6d06b326b3afcdc6a
brien84 <houseinblues@gmail.com>
2023-11-09 23:27:56 +0200

    Create `SessionSetupTests` test case

---
+++ BilliardsTrackerWatchAppTests/SessionSetupTests.swift
@@ -0,0 +1,26 @@
+//
+//  SessionSetupTests.swift
+//  BilliardsTrackerWatchAppTests
+//
+//  Created by Marius on 2023-11-09.
+//
+
+import ComposableArchitecture
+import XCTest
+@testable import BilliardsTrackerWatchApp
+
+final class SessionSetupTests: XCTestCase {
+
+    override func setUp() async throws {
+
+    }
+
+    override func tearDown() async throws {
+
+    }
+
+    func testExample() async throws {
+        XCTAssert(true)
+    }
+
+}
