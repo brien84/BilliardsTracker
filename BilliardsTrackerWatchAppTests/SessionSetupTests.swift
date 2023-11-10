3b6f0a71095d93ef3ce5bfdcbd0297daa1974114
brien84 <houseinblues@gmail.com>
2023-11-10 20:02:38 +0200

    Test shot count selection

--- BilliardsTrackerWatchAppTests/SessionSetupTests.swift
+++ BilliardsTrackerWatchAppTests/SessionSetupTests.swift
@@ -9,18 +9,40 @@ import ComposableArchitecture
 import XCTest
 @testable import BilliardsTrackerWatchApp
 
+@MainActor
 final class SessionSetupTests: XCTestCase {
+    var store: TestStore<SessionSetup.State, SessionSetup.Action, SessionSetup.State, SessionSetup.Action, ()>!
 
     override func setUp() async throws {
-
+        store = TestStore(
+            initialState: SessionSetup.State(mode: .standalone),
+            reducer: SessionSetup()
+        )
     }
 
     override func tearDown() async throws {
-
+        store = nil
     }
 
-    func testExample() async throws {
-        XCTAssert(true)
+    func testNavigatingToShotCount() async throws {
+        await store.send(.setNavigationToShotCount(isActive: true)) {
+            $0.isNavigationToShotCountActive = true
+        }
+
+        await store.send(.setNavigationToShotCount(isActive: false)) {
+            $0.isNavigationToShotCountActive = false
+        }
     }
 
+    func testChangingShotCount() async throws {
+        let newShotCount = 78
+
+        store.dependencies.userDefaults.setOptionsFor = { @Sendable _, options in
+            XCTAssertEqual(options.shotCount, newShotCount)
+        }
+
+        await store.send(.shotCountDidChange(newShotCount)) {
+            $0.options.shotCount = newShotCount
+        }
+    }
 }
