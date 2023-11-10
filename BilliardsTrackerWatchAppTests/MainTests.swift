7583526144fad49badc6a6f9f6b408177edd36c2
brien84 <houseinblues@gmail.com>
2023-11-10 20:02:58 +0200

    Test navigation to `SessionSetup`

--- BilliardsTrackerWatchAppTests/MainTests.swift
+++ BilliardsTrackerWatchAppTests/MainTests.swift
@@ -22,6 +22,26 @@ final class MainTests: XCTestCase {
         store = nil
     }
 
+    func testGettingStandaloneSetupOptionsOnAppear() async throws {
+        let options = SessionOptions(shotCount: 15)
+        store.dependencies.userDefaults.getHasOnboardBeenShown = { @Sendable in true }
+        store.dependencies.userDefaults.getOptionsFor = { @Sendable _ in options }
+
+        await store.send(.onAppear) {
+            $0.standaloneSetup.options = options
+        }
+    }
+
+    func testNavigatingtoOnboardOnAppear() async throws {
+        store.dependencies.userDefaults.getHasOnboardBeenShown = { @Sendable in false }
+        store.dependencies.userDefaults.setHasOnboardBeenShown = { @Sendable _ in }
+        store.dependencies.userDefaults.getOptionsFor = { @Sendable _ in SessionOptions() }
+
+        await store.send(.onAppear) {
+            $0.isNavigationToOnboardActive = true
+        }
+    }
+
     func testNavigatingToOnboard() async throws {
         await store.send(.setNavigationToOnboard(isActive: true)) {
             $0.isNavigationToOnboardActive = true
@@ -32,18 +52,26 @@ final class MainTests: XCTestCase {
         }
     }
 
-    func testNavigatingtoOnboardOnAppear() async throws {
-        store.dependencies.userDefaults.getHasOnboardBeenShown = { @Sendable in false }
-        store.dependencies.userDefaults.setHasOnboardBeenShown = { @Sendable _ in }
+    func testNavigatingToStandaloneSetup() async throws {
+        await store.send(.setNavigationToStandaloneSetup(isActive: true)) {
+            $0.isNavigationToStandaloneSetupActive = true
+        }
 
-        await store.send(.onAppear) {
-            $0.isNavigationToOnboardActive = true
+        await store.send(.setNavigationToStandaloneSetup(isActive: false)) {
+            $0.isNavigationToStandaloneSetupActive = false
         }
     }
 
-    func navigatingToStandaloneSession() async throws {
+    func testNavigatingToStandaloneSession() async throws {
+        let options = SessionOptions(shotCount: 64)
+
+        let store = TestStore(
+            initialState: Main.State(standaloneSetup: SessionSetup.State(mode: .standalone, options: options)),
+            reducer: Main()
+        )
+
         await store.send(.setNavigationToStandalone(isActive: true)) {
-            $0.standalone = Session.State(title: "Standalone", shotCount: 9, isContinuous: true)
+            $0.standalone = Session.State(title: "Standalone", shotCount: options.shotCount!, isContinuous: true)
             $0.isNavigationToStandaloneActive = true
         }
     }
